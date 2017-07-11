69b26c73a9718f240e82ac9343bd3a117920e0df
Robin Aggleton <robin.aggleton@cern.ch>
2017-07-11 16:10:21 +0200

    Add line at 1 for ratio subplot

--- comparator.py
+++ comparator.py
@@ -425,7 +425,8 @@ class Plot(object):
             if self.subplot_type == "ratio":
                 # self.subplot_container.SetMinimum(self.subplot_ratio_lim[0])  # use this, not SetRangeUser()
                 # self.subplot_container.SetMaximum(self.subplot_ratio_lim[1])
-                self.subplot_line = ROOT.TLine(0., 1., 1., 1.,)
+                xax = modifier.GetXaxis()
+                self.subplot_line = ROOT.TLine(xax.GetXmin(), 1., xax.GetXmax(), 1.)
                 self.subplot_line.SetLineStyle(2)
                 self.subplot_line.SetLineWidth(2)
                 self.subplot_line.SetLineColor(ROOT.kBlack)
