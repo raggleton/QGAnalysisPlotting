7a45d90c2b7eaeacc6c65abd50c97c0a4ddb3fde
Robin Aggleton <robin.aggleton@cern.ch>
2017-10-18 11:35:38 +0200

    Split response batch script into individual and comparison parts for flexibility

--- do_all_response_comparisons.sh
+++ do_all_response_comparisons.sh
@@ -4,26 +4,42 @@
 
 CMD="./print_jet_response_qg_plots.py"
 
-# PUS
-for R in 4 8;
-do
-    $CMD workdir_ak${R}puppi_mgpythia workdir_ak${R}chs_mgpythia --output workdir_ak${R}puppi_mgpythia/response_plots_comparePUS
-done
+function doComparisons {
+    # PUS
+    for R in 4 8;
+    do
+        $CMD workdir_ak${R}puppi_mgpythia workdir_ak${R}chs_mgpythia --output workdir_ak${R}puppi_mgpythia/response_plots_comparePUS
+    done
 
-# jet R
-for PUS in chs puppi;
-do
-    $CMD workdir_ak4${PUS}_mgpythia workdir_ak8${PUS}_mgpythia --output workdir_ak4${PUS}_mgpythia/response_plots_compareR
-done
-
-# generator
-for R in 4 8; 
-do
+    # jet R
     for PUS in chs puppi;
     do
-        $CMD workdir_ak${R}${PUS}_mgpythia workdir_ak${R}${PUS}_herwig_reweight --output workdir_ak${R}${PUS}_herwig_reweight/response_plots_compareGenerator
-        # individual ones
-        $CMD workdir_ak${R}${PUS}_mgpythia
-        $CMD workdir_ak${R}${PUS}_herwig_reweight
+        $CMD workdir_ak4${PUS}_mgpythia workdir_ak8${PUS}_mgpythia --output workdir_ak4${PUS}_mgpythia/response_plots_compareR
     done
-done
+
+    # generator
+    for R in 4 8; 
+    do
+        for PUS in chs puppi;
+        do
+            $CMD workdir_ak${R}${PUS}_mgpythia workdir_ak${R}${PUS}_herwig_reweight --output workdir_ak${R}${PUS}_herwig_reweight/response_plots_compareGenerator
+        done
+    done
+}
+
+function doIndividuals {
+    # individual ones
+    for R in 4 8; 
+    do
+        for PUS in chs puppi;
+        do
+            for GEN in mgpythia herwig_reweight;
+            do
+                $CMD workdir_ak${R}${PUS}_${GEN}
+            done
+        done
+    done
+}
+
+# doComparisons;
+doIndividuals;
