7c1e83303033eeaac74ffbab693580dec862253b
Robin Aggleton <robin.aggleton@cern.ch>
2018-08-07 23:41:29 +0200

    Add AK8 bin edges and nicer printout of all bins

--- calc_pt_edges.py
+++ calc_pt_edges.py
@@ -8,6 +8,8 @@ such that the bin width is guaranteed to be a certain fraction of the bin edge
 
 from math import log
 
+# AK4
+print("Doing AK4")
 edges = [
 50,
 65,
@@ -21,9 +23,25 @@ edges = [
 535,
 ]
 
+# AK8
+# print("Doing AK8")
+# edges = [
+# 91,
+# 112,
+# 135,
+# 215,
+# 290,
+# 359,
+# 433,
+# 533,
+# 580
+# ]
+
 # fractional bin width cannot be below this
 # lambda so you can do it as a function of pt
 min_multiplier = lambda x: 1.25
+
+all_bins = []
 
 for low, high in zip(edges[:-1], edges[1:]):
     smallest_multiplier = 9999
@@ -41,4 +59,8 @@ for low, high in zip(edges[:-1], edges[1:]):
         compromise_str = "** COMPROMISE, requires multiplier = %.3f" % (high/low)
 
     this_bin_edges = [round(low*pow(smallest_multiplier, i)) for i in range(0, N_bins+1)]
+    all_bins += this_bin_edges
     print("multiplier: %.3f" % smallest_multiplier, "gives", N_bins, "bins:", this_bin_edges, compromise_str)
+
+# set cos I'm lazy and didnt filter out repeats
+print("All bins:", sorted(list(set(all_bins))))
