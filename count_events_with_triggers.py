ba491aada85718bd515d7fce6584de2e8748de9e
Robin Aggleton <robin.aggleton@cern.ch>
2018-08-25 11:54:13 +0200

    Add cumulative plots to trig counting script; refactor

--- count_events_with_triggers.py
+++ count_events_with_triggers.py
@@ -145,64 +145,69 @@ TOTAL_LUMI = 35918219492.947
 ZB_LUMI = 29048.362
 
 
-def do_counting(zb_input_file, jetht_input_file, trig_info, eta_min=-2.4, eta_max=2.4, title="", output_filename="pt.pdf"):
-    # first get num event passing ZeroBias trigger, up to the first trigger threshold
-    h_zb = cu.get_from_tfile(zb_input_file, "ZeroBiasRef/pt_vs_eta_all")
+def get_xprojection_hist(h2d, xmin, xmax, ymin, ymax):
+    """Get X projection of TH2, with cut on x values as well"""
+    # Need to use a TCutG to do x axis cut
+    cut_name = h2d.GetName()+"cut"
+    this_cut = ROOT.TCutG(cut_name, 5)
+    this_cut.SetPoint(0, xmin, ymin)
+    this_cut.SetPoint(1, xmax, ymin)
+    this_cut.SetPoint(2, xmax, ymax)
+    this_cut.SetPoint(3, xmin, ymax)
+    this_cut.SetPoint(4, xmin, ymin)
 
     # Figure out bins for eta edges
-    eta_min_bin, eta_max_bin = 0, h_zb.GetNbinsY()+1
-    yax = h_zb.GetYaxis()
+    ymin_bin, ymax_bin = 0, h2d.GetNbinsY()+1
+    yax = h2d.GetYaxis()
 
-    eta_min_bin = yax.FindBin(eta_min)
-    eta_max_bin = yax.FindBin(eta_max)
+    ymin_bin = yax.FindBin(ymin)
+    ymax_bin = yax.FindBin(ymax)
     # don't want to include the upper bin if the value is at the low edge
-    if yax.GetBinLowEdge(eta_max_bin) == eta_max:
-        eta_max_bin -= 1
+    if yax.GetBinLowEdge(ymax_bin) == ymax:
+        ymax_bin -= 1
 
-    # Cut for this region of eta and pt phase space
-    pt_min = 50
+    h1d = h2d.ProjectionX(cu.get_unique_str(), ymin_bin, ymax_bin, "["+cut_name+"]")
+    h1d.Sumw2()
+    return h1d
+
+
+def do_counting(zb_input_file, jetht_input_file, trig_info, eta_min=-2.4, eta_max=2.4, title="", output_filename="pt.pdf"):
+    """Get hists of pt spectrum for each trigger, plot, and make & return cumulative hist"""
+    
     trig_names = list(trig_info.keys())
-    pt_max = trig_info[trig_names[0]]['actual_threshold']
-    cut_name = "zbcut"
-    this_pt_cut = ROOT.TCutG(cut_name, 5)
-    this_pt_cut.SetPoint(0, pt_min, eta_min)
-    this_pt_cut.SetPoint(1, pt_max, eta_min)
-    this_pt_cut.SetPoint(2, pt_max, eta_max)
-    this_pt_cut.SetPoint(3, pt_min, eta_max)
-    this_pt_cut.SetPoint(4, pt_min, eta_min)
-
-    h_zb_pt = h_zb.ProjectionX("ZeroBias", eta_min_bin, eta_max_bin, "["+cut_name+"]")
+    
+    # first get num event passing ZeroBias trigger, up to the first trigger threshold
+    h_zb = cu.get_from_tfile(zb_input_file, "ZeroBiasRef/pt_vs_eta_all")
+    pt_min, pt_max = 50, trig_info[trig_names[0]]['actual_threshold']
+    h_zb_pt = get_xprojection_hist(h_zb, 50, pt_max, eta_min, eta_max)
+    h_zb_pt.SetName("ZeroBias")
     h_zb_pt.Sumw2()
-    h_zb_pt.SetFillColor(17)
-    h_zb_pt.SetLineColor(17)
+    zb_color = 17
+    h_zb_pt.SetFillColor(zb_color)
+    h_zb_pt.SetLineColor(zb_color)
 
     print("WARNING: rounding to nearest bin width", h_zb_pt.GetBinWidth(1))
 
     hst = ROOT.THStack("hst", title+";Leading jet p_{T} [GeV];N")
     hst.Add(h_zb_pt)
 
+    # for printout at end
     total = []
     this_num_entries = h_zb_pt.Integral()
     total.append(("ZeroBias", this_num_entries))
 
-    leg = ROOT.TLegend(0.6, 0.6, 0.88, 0.88)
-    leg.AddEntry(h_zb_pt, "ZeroBias (p_{T}>%g GeV): %g" % (pt_min, this_num_entries), "LF")
+    leg = ROOT.TLegend(0.6, 0.5, 0.95, 0.88)
+    leg.AddEntry(h_zb_pt, "ZeroBias (p_{T} > %g GeV): %g" % (pt_min, this_num_entries), "LF")
 
-    # Now go through each of the triggers and get their hists
+    # Now go through each of the triggers and get their hists, 
+    # using next trigger actual threshold as upper limit on pt
     for this_trig_name, next_trig_name in zip(trig_names[:-1], trig_names[1:]):
         h2d = cu.get_from_tfile(jetht_input_file, this_trig_name+"_v*Ref/pt_vs_eta_all")
+        
         pt_min = trig_info[this_trig_name]['actual_threshold']
         pt_max = trig_info[next_trig_name]['actual_threshold']
-        cut_name = this_trig_name+"cut"
-        this_pt_cut = ROOT.TCutG(cut_name, 5)
-        this_pt_cut.SetPoint(0, pt_min, eta_min)
-        this_pt_cut.SetPoint(1, pt_max, eta_min)
-        this_pt_cut.SetPoint(2, pt_max, eta_max)
-        this_pt_cut.SetPoint(3, pt_min, eta_max)
-        this_pt_cut.SetPoint(4, pt_min, eta_min)
-
-        h_pt = h2d.ProjectionX(this_trig_name, eta_min_bin, eta_max_bin, "["+cut_name+"]")
-        h_pt.Sumw2()
+        h_pt = get_xprojection_hist(h2d, pt_min, pt_max, eta_min, eta_max)
+        h_pt.SetName(this_trig_name)
         h_pt.SetFillColor(trig_info[this_trig_name]['color'])
         h_pt.SetLineColor(trig_info[this_trig_name]['color'])
 
@@ -212,22 +217,12 @@ def do_counting(zb_input_file, jetht_input_file, trig_info, eta_min=-2.4, eta_ma
         hst.Add(h_pt)
         leg.AddEntry(h_pt, this_trig_name + ": %g" % (this_num_entries), "LF")
 
-
     # Do the last trigger manually
     last_trig_name = trig_names[-1]
     h2d = cu.get_from_tfile(jetht_input_file, last_trig_name+"_v*Ref/pt_vs_eta_all")
     pt_min = trig_info[last_trig_name]['actual_threshold']
-    pt_max = 10000
-    cut_name = last_trig_name+"cut"
-    this_pt_cut = ROOT.TCutG(cut_name, 5)
-    this_pt_cut.SetPoint(0, pt_min, eta_min)
-    this_pt_cut.SetPoint(1, pt_max, eta_min)
-    this_pt_cut.SetPoint(2, pt_max, eta_max)
-    this_pt_cut.SetPoint(3, pt_min, eta_max)
-    this_pt_cut.SetPoint(4, pt_min, eta_min)
-
-    h_pt = h2d.ProjectionX(last_trig_name, eta_min_bin, eta_max_bin, "["+cut_name+"]")
-    h_pt.Sumw2()
+    h_pt = get_xprojection_hist(h2d, pt_min, 10000, eta_min, eta_max)
+    h_pt.SetName(last_trig_name)
     h_pt.SetFillColor(trig_info[last_trig_name]['color'])
     h_pt.SetLineColor(trig_info[last_trig_name]['color'])
 
@@ -237,13 +232,16 @@ def do_counting(zb_input_file, jetht_input_file, trig_info, eta_min=-2.4, eta_ma
     hst.Add(h_pt)
     leg.AddEntry(h_pt, last_trig_name + ": %g" % (this_num_entries), "LF")
 
+    # Now print a plot showing distribution of each trigger
     c = ROOT.TCanvas("c", "", 800, 600)
     c.SetTicks(1, 1)
     # c.SetLogx()
     c.SetLogy()
+
     hst.Draw("HISTE NOSTACK")
     hst.SetMinimum(10)
-    hst.SetMaximum(1000000)
+    hst.SetMaximum(1E6)
+    
     total_count = sum(x[1] for x in total)
     leg.AddEntry(0, "Total: "+str(total_count), "")
     leg.Draw()
@@ -253,6 +251,53 @@ def do_counting(zb_input_file, jetht_input_file, trig_info, eta_min=-2.4, eta_ma
         print(count[0], "=", count[1])
     print("Total:", total_count)
 
+    # Now make a cumulative histogram of all triggers, need to convert to
+    # single TH1 first though from THStack
+    htotal = None
+    hists = hst.GetHists()
+    for h in hists:
+        if htotal is None:
+            htotal = h.Clone("total")
+        else:
+            htotal.Add(h)
+
+    cumul = htotal.GetCumulative()
+    cumul.SetTitle(title+" (cumulative);Leading jet p_{T} [GeV];N")
+    c.Clear()
+    c.SetLogx()
+    cumul.SetFillStyle(0)
+    cumul.SetLineColor(ROOT.kRed)
+    cumul.Draw("HISTE")
+    cumul.SetMinimum(5E5)
+    cumul.SetMaximum(5E7)
+    cumul.SetAxisRange(50, 2000, "X")
+    c.SaveAs(output_filename.replace(".pdf", "_cumul.pdf"))
+
+    return cumul
+
+
+def print_cumulative_comparison(entries, output_filename):
+    """Print comparison of cumulative distributions
+
+    entries is a list of [TH1, label] pairs
+    """
+    c = ROOT.TCanvas("comp", "", 800, 600)
+    c.SetTicks(1, 1)
+    c.SetLogx()
+    c.SetLogy()
+
+    hst_cumul = ROOT.THStack("hst_cumul", "Cumulative distributions;Leading jet p_{T} [GeV];N")
+    leg_cumul = ROOT.TLegend(0.15, 0.6, 0.45, 0.88)
+
+    for hist, label in entries:
+        hst_cumul.Add(hist)
+        leg_cumul.AddEntry(hist, label, "L")
+
+    hst_cumul.Draw("NOSTACKE HISTE")
+    hst_cumul.GetXaxis().SetRangeUser(50, 2000)
+    leg_cumul.Draw()
+    c.SaveAs(output_filename)
+
 
 if __name__ == "__main__":
     
@@ -260,7 +305,19 @@ if __name__ == "__main__":
     jetht_file = cu.open_root_file('workdir_ak8puppi_jettrig_withAK8trig/uhh2.AnalysisModuleRunner.DATA.Data_JetHT.root')
 
     # count for normal triggers
-    do_counting(zb_file, jetht_file, normal_trig_info, -2.4, 2.4, "AK8PUPPI + HLT_PFJet*", "workdir_ak8puppi_jettrig_withAK8trig/ak8puppi_normaltrig.pdf")
-    
+    cumul_normal = do_counting(zb_file, jetht_file, normal_trig_info, -2.4, 2.4, "AK8PUPPI + HLT_PFJet*", "workdir_ak8puppi_jettrig_withAK8trig/ak8puppi_normaltrig.pdf")
+    cumul_normal.SetLineColor(ROOT.kBlue)
+    cumul_normal.SetLineStyle(2)
+
     # count for AK8 specific triggers
-    do_counting(zb_file, jetht_file, ak8_trig_info, -2.4, 2.4, "AK8PUPPI + HLT_AK8PFJet*", "workdir_ak8puppi_jettrig_withAK8trig/ak8puppi_ak8trig.pdf")
+    cumul_ak8 = do_counting(zb_file, jetht_file, ak8_trig_info, -2.4, 2.4, "AK8PUPPI + HLT_AK8PFJet*", "workdir_ak8puppi_jettrig_withAK8trig/ak8puppi_ak8trig.pdf")
+
+    print_cumulative_comparison([
+            [cumul_normal, "HLT_PFJet*"],
+            [cumul_ak8, "HLT_AK8PFJet*"]
+        ],
+        output_filename="workdir_ak8puppi_jettrig_withAK8trig/compare_cumul.pdf"
+    )
+
+    zb_file.Close()
+    jetht_file.Close()
