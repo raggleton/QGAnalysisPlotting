4e8b11f0624461e66880d22e95c0f465a7fa8c0f
Robin Aggleton <robin.aggleton@cern.ch>
2018-10-12 08:32:33 +0200

    Fix top padding differences with/without subplot

--- comparator.py
+++ comparator.py
@@ -408,13 +408,14 @@ class Plot(object):
                 self.canvas = ROOT.TCanvas(ROOT.TUUID().AsString(), "", *self.default_canvas_size)
                 self.canvas.SetTicks(1, 1)
                 right_margin = 0.03
-                if self.subplot:
+                top_margin = 0.1
+                if self.subplot_type:
                     self.main_pad = ROOT.TPad("main_pad", "", 0, self.subplot_pad_height+self.subplot_pad_fudge, 1, 1)
                     ROOT.SetOwnership(self.main_pad, False)
                     self.main_pad.SetTicks(1, 1)
                     self.main_pad.SetBottomMargin(2*self.subplot_pad_fudge)
-                    self.main_pad.SetTopMargin(0.12)
-                    # self.main_pad.SetRightMargin(right_margin)
+                    self.main_pad.SetTopMargin(top_margin / (1-self.subplot_pad_height))
+                    self.main_pad.SetRightMargin(right_margin / (1-self.subplot_pad_height))
                     self.canvas.cd()
                     self.main_pad.Draw()
                     self.subplot_pad = ROOT.TPad("subplot_pad", "", 0, 0, 1, self.subplot_pad_height-self.subplot_pad_fudge)
@@ -431,6 +432,7 @@ class Plot(object):
                     self.main_pad = ROOT.TPad("main_pad", "", 0, 0, 1, 1)
                     ROOT.SetOwnership(self.main_pad, False)
                     self.main_pad.SetRightMargin(right_margin)
+                    self.main_pad.SetTopMargin(top_margin)
                     self.main_pad.SetTicks(1, 1)
                     self.main_pad.Draw()
 
