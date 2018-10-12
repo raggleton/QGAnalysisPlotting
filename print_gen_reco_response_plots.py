5645699be9ec7a0d043bb1666e29e06f85bbe86b
Robin Aggleton <robin.aggleton@cern.ch>
2018-10-12 18:53:25 +0200

    Update gen reco response plots to handle other variables

--- print_gen_reco_response_plots.py
+++ print_gen_reco_response_plots.py
@@ -30,74 +30,7 @@ ROOT.gStyle.SetPaintTextFormat(".3f")
 OUTPUT_FMT = "pdf"
 
 
-def do_pt_response_plot(tdir, plot_dir):
-    """Do 2D plots of genjet pt vs recojet pt"""
-    h2d = cu.get_from_tfile(tdir, "pt_jet_response")
-    
-    #  renorm by row (bins of pTReco)
-    h2d_renorm_y = cu.make_normalised_TH2(h2d, 'Y', recolour=False)
-    canv = ROOT.TCanvas(cu.get_unique_str(), "", 800, 600)
-    pad = ROOT.gPad
-    pad.SetBottomMargin(0.12)
-    pad.SetLeftMargin(0.13)
-    pad.SetRightMargin(0.12)
-    canv.SetTicks(1, 1)
-    canv.SetLogx()
-    canv.SetLogy()
-    h2d_renorm_y.SetMarkerSize(0.5)
-    h2d_renorm_y.SetMaximum(1)
-    h2d_renorm_y.Draw("COLZ TEXT")
-    xax = h2d_renorm_y.GetXaxis()
-    upper_lim = h2d_renorm_y.GetXaxis().GetBinUpEdge(h2d_renorm_y.GetXaxis().GetLast())
-    upper_lim = 5000
-    title_offset = 1.5
-    h2d_renorm_y.SetTitleOffset(title_offset, 'X')
-    xax.SetRangeUser(10, upper_lim)
-    xax.SetMoreLogLabels()
-
-    yax = h2d_renorm_y.GetYaxis()
-    h2d_renorm_y.SetTitleOffset(title_offset*1.15, 'Y')
-    yax.SetRangeUser(10, upper_lim)
-    yax.SetMoreLogLabels()
-    canv.Update()
-
-    plot_dir = os.path.join(plot_dir, tdir.GetName())
-    cu.check_dir_exists_create(plot_dir)
-    canv.SaveAs(os.path.join(plot_dir, "pt_jet_response_renormY_linZ.%s" % (OUTPUT_FMT)))
-    canv.SetLogz()
-    h2d_renorm_y.SetMaximum(1)
-    h2d_renorm_y.SetMinimum(1E-3)
-    canv.SaveAs(os.path.join(plot_dir, "pt_jet_response_renormY_logZ.%s" % (OUTPUT_FMT)))
-
-    # renorm by column (bins of pTGen)
-    canv.Clear()
-    canv.SetLogz(0)
-    h2d_renorm_x = cu.make_normalised_TH2(h2d, 'X', recolour=False)
-    h2d_renorm_x.SetMarkerSize(0.5)
-    h2d_renorm_x.SetMaximum(1)
-    h2d_renorm_x.Draw("COLZ TEXT")
-
-    xax = h2d_renorm_x.GetXaxis()
-    upper_lim = h2d_renorm_x.GetXaxis().GetBinUpEdge(h2d_renorm_x.GetXaxis().GetLast())
-    upper_lim = 5000
-    title_offset = 1.5
-    h2d_renorm_x.SetTitleOffset(title_offset, 'X')
-    xax.SetRangeUser(10, upper_lim)
-    xax.SetMoreLogLabels()
-
-    yax = h2d_renorm_x.GetYaxis()
-    h2d_renorm_x.SetTitleOffset(title_offset*1.15, 'Y')
-    yax.SetRangeUser(10, upper_lim)
-    yax.SetMoreLogLabels()
-    canv.Update()
-    canv.SaveAs(os.path.join(plot_dir, "pt_jet_response_renormX_linZ.%s" % (OUTPUT_FMT)))
-    canv.SetLogz()
-    h2d_renorm_x.SetMaximum(1)
-    h2d_renorm_x.SetMinimum(1E-3)
-    canv.SaveAs(os.path.join(plot_dir, "pt_jet_response_renormX_logZ.%s" % (OUTPUT_FMT)))
-
-
-    # Now do plot of purity, etc
+def do_migration_summary_plot(h2d_renorm_x, h2d_renorm_y, xlabel, output_filename, log_var=False):
     stability_values, stability_errs = [], []
     xfer_down_values, xfer_down_errs = [], []
     xfer_down2_values, xfer_down2_errs = [], []
@@ -106,36 +39,38 @@ def do_pt_response_plot(tdir, plot_dir):
 
     binning = [h2d_renorm_x.GetXaxis().GetBinLowEdge(bin_ind) for bin_ind in range(1, h2d_renorm_x.GetNbinsX()+2)]
 
-    hist_purity = ROOT.TH1F("purity"+cu.get_unique_str(), ";p_{T}^{Gen} [GeV];Fraction", len(binning)-1, array('d', binning)) 
-    hist_stability = ROOT.TH1F("stability"+cu.get_unique_str(), ";p_{T}^{Gen} [GeV];Fraction", len(binning)-1, array('d', binning)) 
-    hist_xfer_down = ROOT.TH1F("xfer_down"+cu.get_unique_str(), ";p_{T}^{Gen} [GeV];Fraction", len(binning)-1, array('d', binning)) 
-    hist_xfer_down2 = ROOT.TH1F("xfer_down2"+cu.get_unique_str(), ";p_{T}^{Gen} [GeV];Fraction", len(binning)-1, array('d', binning)) 
-    hist_xfer_down3 = ROOT.TH1F("xfer_down3"+cu.get_unique_str(), ";p_{T}^{Gen} [GeV];Fraction", len(binning)-1, array('d', binning)) 
-    hist_xfer_up = ROOT.TH1F("xfer_up"+cu.get_unique_str(), ";p_{T}^{Gen} [GeV];Fraction", len(binning)-1, array('d', binning)) 
-    hist_xfer_up2 = ROOT.TH1F("xfer_up2"+cu.get_unique_str(), ";p_{T}^{Gen} [GeV];Fraction", len(binning)-1, array('d', binning)) 
-    hist_xfer_up3 = ROOT.TH1F("xfer_up3"+cu.get_unique_str(), ";p_{T}^{Gen} [GeV];Fraction", len(binning)-1, array('d', binning)) 
+    n_bins = len(binning) - 1
+    label = ";%s;Fraction" % xlabel
+    hist_purity = ROOT.TH1F("purity"+cu.get_unique_str(), xlabel, n_bins, array('d', binning))
+    hist_stability = ROOT.TH1F("stability"+cu.get_unique_str(), xlabel, n_bins, array('d', binning))
+    hist_xfer_down = ROOT.TH1F("xfer_down"+cu.get_unique_str(), xlabel, n_bins, array('d', binning))
+    hist_xfer_down2 = ROOT.TH1F("xfer_down2"+cu.get_unique_str(), xlabel, n_bins, array('d', binning))
+    hist_xfer_down3 = ROOT.TH1F("xfer_down3"+cu.get_unique_str(), xlabel, n_bins, array('d', binning))
+    hist_xfer_up = ROOT.TH1F("xfer_up"+cu.get_unique_str(), xlabel, n_bins, array('d', binning))
+    hist_xfer_up2 = ROOT.TH1F("xfer_up2"+cu.get_unique_str(), xlabel, n_bins, array('d', binning))
+    hist_xfer_up3 = ROOT.TH1F("xfer_up3"+cu.get_unique_str(), xlabel, n_bins, array('d', binning))
 
     for bin_ind in range(1, h2d_renorm_x.GetNbinsX()+1):
         hist_purity.SetBinContent(bin_ind, h2d_renorm_y.GetBinContent(bin_ind, bin_ind))
         hist_purity.SetBinError(bin_ind, h2d_renorm_y.GetBinError(bin_ind, bin_ind))
-        
+
         hist_stability.SetBinContent(bin_ind, h2d_renorm_x.GetBinContent(bin_ind, bin_ind))
         hist_stability.SetBinError(bin_ind, h2d_renorm_x.GetBinError(bin_ind, bin_ind))
-        
+
         hist_xfer_down.SetBinContent(bin_ind, h2d_renorm_x.GetBinContent(bin_ind, bin_ind-1))
         hist_xfer_down.SetBinError(bin_ind, h2d_renorm_x.GetBinError(bin_ind, bin_ind-1))
         hist_xfer_down2.SetBinContent(bin_ind, h2d_renorm_x.GetBinContent(bin_ind, bin_ind-2))
         hist_xfer_down2.SetBinError(bin_ind, h2d_renorm_x.GetBinError(bin_ind, bin_ind-2))
         hist_xfer_down3.SetBinContent(bin_ind, h2d_renorm_x.GetBinContent(bin_ind, bin_ind-3))
         hist_xfer_down3.SetBinError(bin_ind, h2d_renorm_x.GetBinError(bin_ind, bin_ind-3))
-        
+
         hist_xfer_up.SetBinContent(bin_ind, h2d_renorm_x.GetBinContent(bin_ind, bin_ind+1))
         hist_xfer_up.SetBinError(bin_ind, h2d_renorm_x.GetBinError(bin_ind, bin_ind+1))
         hist_xfer_up2.SetBinContent(bin_ind, h2d_renorm_x.GetBinContent(bin_ind, bin_ind+2))
         hist_xfer_up2.SetBinError(bin_ind, h2d_renorm_x.GetBinError(bin_ind, bin_ind+2))
         hist_xfer_up3.SetBinContent(bin_ind, h2d_renorm_x.GetBinContent(bin_ind, bin_ind+3))
         hist_xfer_up3.SetBinError(bin_ind, h2d_renorm_x.GetBinError(bin_ind, bin_ind+3))
-   
+
     # col_purity = ROOT.kViolet-3
     # col_purity = ROOT.kOrange-3
     col_purity = ROOT.kGreen+1
@@ -156,13 +91,17 @@ def do_pt_response_plot(tdir, plot_dir):
         Contribution(hist_xfer_up2, label="2 higher reco bin", line_color=col_xfer_up2, marker_color=col_xfer_up2),
         # Contribution(hist_xfer_up3, label="3 higher reco bin", line_color=col_xfer_up3, marker_color=col_xfer_up3),
     ]
-    xlim = [10, binning[-1]]
-    plot = Plot(contributions, what='hist', xlim=xlim, ylim=[1e-4, 2])
+    xlim = [binning[0], binning[-1]]
+    plot = Plot(contributions, what='hist', xlim=xlim, ylim=[1e-4, 2], xtitle=xlabel)
     y1 = 0.15
+    plot.legend.SetX1(0.5)
     plot.legend.SetY1(y1)
-    plot.legend.SetY2(y1+0.2)
+    plot.legend.SetY2(y1+0.25)
     plot.plot("NOSTACK HISTE")
-    plot.set_logx()
+    plot.legend.SetFillStyle(1001)
+    plot.legend.SetFillColorAlpha(ROOT.kWhite, 0.75)
+    if log_var:
+        plot.set_logx()
     plot.set_logy()
     plot.main_pad.cd()
     lines = []
@@ -171,7 +110,86 @@ def do_pt_response_plot(tdir, plot_dir):
         line.SetLineStyle(2)
         lines.append(line)
         line.Draw("same")
-    plot.save(os.path.join(plot_dir, 'migration_summary.%s' % (OUTPUT_FMT)))
+    plot.save(output_filename)
+
+
+def do_response_plot(tdir, plot_dir, var_name, xlabel, log_var=False, rebin=1):
+    """Do 2D plots of genjet pt vs recojet pt"""
+    h2d = cu.get_from_tfile(tdir, "%s_response" % var_name)
+    h2d.RebinX(rebin)
+    h2d.RebinY(rebin)
+
+    #  renorm by row (reco bins)
+    h2d_renorm_y = cu.make_normalised_TH2(h2d, 'Y', recolour=False, do_errors=True)
+    canv = ROOT.TCanvas(cu.get_unique_str(), "", 800, 600)
+    pad = ROOT.gPad
+    pad.SetBottomMargin(0.12)
+    pad.SetLeftMargin(0.13)
+    pad.SetRightMargin(0.12)
+    canv.SetTicks(1, 1)
+    if log_var:
+        canv.SetLogx()
+        canv.SetLogy()
+    h2d_renorm_y.SetMarkerSize(0.5)
+    h2d_renorm_y.SetMaximum(1)
+    draw_opt = "COLZ TEXT"
+    draw_opt = "COLZ"
+    h2d_renorm_y.Draw(draw_opt)
+    xax = h2d_renorm_y.GetXaxis()
+    upper_lim = h2d_renorm_y.GetXaxis().GetBinUpEdge(h2d_renorm_y.GetXaxis().GetLast())
+    # upper_lim = 5000
+    title_offset = 1.5
+    h2d_renorm_y.SetTitleOffset(title_offset, 'X')
+    xax.SetRangeUser(0, upper_lim)
+    xax.SetMoreLogLabels()
+
+    yax = h2d_renorm_y.GetYaxis()
+    h2d_renorm_y.SetTitleOffset(title_offset*1.15, 'Y')
+    yax.SetRangeUser(0, upper_lim)
+    yax.SetMoreLogLabels()
+    canv.Update()
+
+    plot_dir = os.path.join(plot_dir, tdir.GetName())
+    cu.check_dir_exists_create(plot_dir)
+    canv.SaveAs(os.path.join(plot_dir, "%s_response_renormY_linZ.%s" % (var_name, OUTPUT_FMT)))
+    canv.SetLogz()
+    h2d_renorm_y.SetMaximum(1)
+    h2d_renorm_y.SetMinimum(1E-3)
+    canv.SaveAs(os.path.join(plot_dir, "%s_response_renormY_logZ.%s" % (var_name, OUTPUT_FMT)))
+
+    # renorm by column (gen bins)
+    canv.Clear()
+    canv.SetLogz(0)
+    h2d_renorm_x = cu.make_normalised_TH2(h2d, 'X', recolour=False, do_errors=True)
+    h2d_renorm_x.SetMarkerSize(0.5)
+    h2d_renorm_x.SetMaximum(1)
+    h2d_renorm_x.Draw(draw_opt)
+
+    xax = h2d_renorm_x.GetXaxis()
+    upper_lim = h2d_renorm_x.GetXaxis().GetBinUpEdge(h2d_renorm_x.GetXaxis().GetLast())
+    # upper_lim = 5000
+    title_offset = 1.5
+    h2d_renorm_x.SetTitleOffset(title_offset, 'X')
+    xax.SetRangeUser(0, upper_lim)
+    xax.SetMoreLogLabels()
+
+    yax = h2d_renorm_x.GetYaxis()
+    h2d_renorm_x.SetTitleOffset(title_offset*1.15, 'Y')
+    yax.SetRangeUser(0, upper_lim)
+    yax.SetMoreLogLabels()
+    canv.Update()
+    canv.SaveAs(os.path.join(plot_dir, "%s_response_renormX_linZ.%s" % (var_name, OUTPUT_FMT)))
+    canv.SetLogz()
+    h2d_renorm_x.SetMaximum(1)
+    h2d_renorm_x.SetMinimum(1E-3)
+    canv.SaveAs(os.path.join(plot_dir, "%s_response_renormX_logZ.%s" % (var_name, OUTPUT_FMT)))
+
+    # Now do plot of purity, etc
+    do_migration_summary_plot(h2d_renorm_x,
+                              h2d_renorm_y,
+                              xlabel,
+                              os.path.join(plot_dir, '%s_migration_summary.%s' % (var_name, OUTPUT_FMT)),
+                              log_var)
 
 
 def do_jet_index_plots(tdir, plot_dir):
@@ -222,7 +240,7 @@ def do_pt_transfer_plot(tdir, plot_dir):
     hist_name = "pt_jet_response_binning"
     h = cu.get_from_tfile(tdir, hist_name)
     binning = [h.GetXaxis().GetBinLowEdge(bin_ind) for bin_ind in range(1, h.GetNbinsX()+1)]
-    hist_factors = ROOT.TH1F("hist_factors"+cu.get_unique_str(), ";p_{T}^{Reco} [GeV];Fraction rel to previous bin", len(binning)-1, array('d', binning)) 
+    hist_factors = ROOT.TH1F("hist_factors"+cu.get_unique_str(), ";p_{T}^{Reco} [GeV];Fraction rel to previous bin", len(binning)-1, array('d', binning))
     for bin_ind in range(2, h.GetNbinsX()+1):
         cont = h.GetBinContent(bin_ind)
         cont_prev = h.GetBinContent(bin_ind-1)
@@ -231,7 +249,7 @@ def do_pt_transfer_plot(tdir, plot_dir):
         factor = cont / cont_prev
         hist_factors.SetBinContent(bin_ind, factor)
         hist_factors.SetBinError(bin_ind, 0)
-    
+
     col_purity = ROOT.kBlack
     conts = [
         Contribution(hist_factors, label="Factor relative to previous bin", line_color=col_purity, marker_color=col_purity),
@@ -244,24 +262,20 @@ def do_pt_transfer_plot(tdir, plot_dir):
     plot.save(os.path.join(plot_dir, 'pt_migration_factors.%s' % (OUTPUT_FMT)))
 
 
-def do_response_plots(in_file, plot_dir, do_these_dirs=None):
+def do_response_plots(in_file, plot_dir, do_these=None):
     tfile = cu.open_root_file(in_file)
-    dirs = cu.get_list_of_element_names(tfile)
+    for full_var_name, xlabel, log_var, rebin in do_these:
+        mydir, myvar = full_var_name.split("/")
+        do_response_plot(tfile.Get(mydir), plot_dir=plot_dir, var_name=myvar, xlabel=xlabel, log_var=log_var, rebin=rebin)
 
-    for mydir in dirs:
-        if do_these_dirs and mydir not in do_these_dirs:
-            continue
-        
-        do_pt_response_plot(tfile.Get(mydir), plot_dir=plot_dir)
+        # do_jet_index_plots(tfile.Get(mydir), plot_dir=plot_dir)
 
-        do_jet_index_plots(tfile.Get(mydir), plot_dir=plot_dir)
-
-        do_pt_transfer_plot(tfile.Get(mydir), plot_dir=plot_dir)
+        # do_pt_transfer_plot(tfile.Get(mydir), plot_dir=plot_dir)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('input', 
+    parser.add_argument('input',
                         nargs='+',
                         help='Input ROOT files to process. '
                         'Several dirs can be specified here, separated by a space.')
@@ -273,13 +287,17 @@ if __name__ == "__main__":
         default_plot_dir = os.path.join(os.path.dirname(in_file), "response_"+os.path.splitext(os.path.basename(in_file))[0])
         plot_dir = args.output if args.output else default_plot_dir
         cu.check_dir_exists_create(plot_dir)
-        do_these_dirs = [
-            'Dijet',
-            'Dijet_tighter',
-            'ZPlusJets',
-        ]
-        if "_QCD_" in in_file:
-            do_these_dirs = do_these_dirs[:2]
-        if "_DYJetsToLL_" in in_file:
-            do_these_dirs = do_these_dirs[2:]
-        do_response_plots(in_file, plot_dir=plot_dir, do_these_dirs=do_these_dirs)
+        do_these = None
+
+        if "QCD" in in_file:
+            do_these = [
+                ("Dijet_tighter/pt_jet", "p_{T}^{Gen} [GeV]", True, 1),
+                ("Dijet_QG_tighter/jet_puppiMultiplicity", "PUPPI Multiplicity (#lambda_{0}^{0} (PUPPI))", False, 5),
+                ("Dijet_QG_tighter/jet_multiplicity", "Multiplicity (#lambda_{0}^{0})", False, 5),
+                ("Dijet_QG_tighter/jet_LHA", "LHA (#lambda_{0.5}^{1} (PUPPI))", False, 10),
+            ]
+
+        if "DYJetsToLL" in in_file:
+            pass
+        
+        do_response_plots(in_file, plot_dir=plot_dir, do_these=do_these)
