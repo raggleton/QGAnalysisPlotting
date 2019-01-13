fa4e8a3e87ab04ebac14e9d7cee54da8341ab77d
Robin Aggleton <robin.aggleton@cern.ch>
2019-01-13 15:22:03 +0100

    Add YODA naming to 1D lambda plot extractor

--- rebin_lambda_save.py
+++ rebin_lambda_save.py
@@ -1,34 +1,33 @@
 #!/usr/bin/env python
 
-"""Make 1D lambda hists & rebin according to custom rebinning"""
+"""Make 1D lambda hists & rebin according to custom rebinning. 
+Also set names ready for RIVET/YODA.
+"""
+
 
 import ROOT
-from MyStyle import My_Style
-My_Style.cd()
 import os
 import argparse
 from array import array
-from copy import deepcopy
 
 # My stuff
 import qg_common as qgc
 import qg_general_plots as qgp
 import common_utils as cu
 
-# For debugging
-import sys
-import tracers
-# sys.settrace(tracers.trace_calls)
-# sys.settrace(tracers.trace_calls_detail)
-# sys.settrace(tracers.trace_calls_and_returns)
-
-# import hunter
-# hunter.trace(module='comparator')
-
 ROOT.PyConfig.IgnoreCommandLineOptions = True
 ROOT.gROOT.SetBatch(1)
 ROOT.TH1.SetDefaultSumw2()
 ROOT.gStyle.SetOptStat(0)
+
+
+def create_yoda_hist_name(region, angle_name, pt_bin):
+    """Produce YODA hist name"""
+    region_num = 1 if "dijet" in region.lower() else 2
+    angle_names = [a.var for a in qgc.COMMON_VARS]
+    angle_num = angle_names.index(angle_name)
+    pt_bin_num = qgc.PT_BINS.index(pt_bin)
+    return "d{:02d}-x{:02d}-y{:02d}".format(region_num, angle_num, pt_bin_num)
 
 
 def make_1D_rebin_hists(input_filename, plot_dirname, output_filename):
@@ -38,7 +37,7 @@ def make_1D_rebin_hists(input_filename, plot_dirname, output_filename):
     in_f = cu.open_root_file(input_filename)
     out_f = ROOT.TFile(output_filename, "RECREATE")
 
-    for ang in qgc.COMMON_VARS:
+    for ang_ind, ang in enumerate(qgc.COMMON_VARS):
         if ang.var not in qgc.ANGLE_REBIN_DICT:
             continue
 
@@ -51,7 +50,8 @@ def make_1D_rebin_hists(input_filename, plot_dirname, output_filename):
         for pt_ind, (start_val, end_val) in enumerate(qgc.PT_BINS):
             hist = qgp.get_projection_plot(h2d, start_val, end_val)
             this_rebins = qgc.ANGLE_REBIN_DICT[ang.var]
-            new_name = "%s_Pt%sto%d" % (ang.var, start_val, end_val)
+            # new_name = "%s_Pt%sto%d" % (ang.var, start_val, end_val)
+            new_name = create_yoda_hist_name(plot_dirname, ang.var, (start_val, end_val))
             rebin_hist = hist.Rebin(len(this_rebins)-1, new_name, array('d', this_rebins))
             out_f.WriteTObject(rebin_hist)  # saves faffing with cd()
 
@@ -66,4 +66,3 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     make_1D_rebin_hists(args.input, args.plotDirname, args.output)
-
