b375e09324558b09416378858724cc4c1e4319e3
Robin Aggleton <robin.aggleton@cern.ch>
2019-01-14 09:30:18 +0100

    Make plot file as well when extracting plots for yoda, also add in dijet pt hist

--- rebin_lambda_save.py
+++ rebin_lambda_save.py
@@ -25,9 +25,33 @@ def create_yoda_hist_name(region, angle_name, pt_bin):
     """Produce YODA hist name"""
     region_num = 1 if "dijet" in region.lower() else 2
     angle_names = [a.var for a in qgc.COMMON_VARS]
-    angle_num = angle_names.index(angle_name)
-    pt_bin_num = qgc.PT_BINS.index(pt_bin)
+    angle_num = angle_names.index(angle_name)+1
+    pt_bin_num = qgc.PT_BINS.index(pt_bin)+1
     return "d{:02d}-x{:02d}-y{:02d}".format(region_num, angle_num, pt_bin_num)
+
+YODA_PLOT_TEMPLATE = """BEGIN PLOT /CMS_2018_PAS_SMP_18_QGX/{plotname}
+Title=$\sqrt{{s}} = 13$ TeV, $\mathcal{{L}}_{{\mathrm{{int}}}} = 35.9~\mathrm{{fb}}^{{-1}}$, Dijet selection, {title}
+XLabel={xlabel}
+YLabel={ylabel}
+XMin=0
+LegendXPos=0.8
+LegendYPos=0.9
+Legend=1
+ErrorBands=1
+ErrorBars=1
+ErrorBandOpacity=0.5
+END PLOT
+
+"""
+"""
+BEGIN HISTOGRAM /CMS_2018_PAS_SMP_18_QGX/{plotname}
+ErrorBars=1
+ErrorBandOpacity=1
+ErrorType=stat
+PolyMarker=o
+ConnectBins=0
+END HISTOGRAM
+"""
 
 
 def make_1D_rebin_hists(input_filename, plot_dirname, output_filename):
@@ -37,7 +61,9 @@ def make_1D_rebin_hists(input_filename, plot_dirname, output_filename):
     in_f = cu.open_root_file(input_filename)
     out_f = ROOT.TFile(output_filename, "RECREATE")
 
-    for ang_ind, ang in enumerate(qgc.COMMON_VARS):
+    plot_file = open(output_filename.replace(".root", ".plot"), "w")
+
+    for ang_ind, ang in enumerate(qgc.COMMON_VARS[:5]):
         if ang.var not in qgc.ANGLE_REBIN_DICT:
             continue
 
@@ -51,11 +77,35 @@ def make_1D_rebin_hists(input_filename, plot_dirname, output_filename):
             hist = qgp.get_projection_plot(h2d, start_val, end_val)
             this_rebins = qgc.ANGLE_REBIN_DICT[ang.var]
             # new_name = "%s_Pt%sto%d" % (ang.var, start_val, end_val)
-            new_name = create_yoda_hist_name(plot_dirname, ang.var, (start_val, end_val))
-            rebin_hist = hist.Rebin(len(this_rebins)-1, new_name, array('d', this_rebins))
+            yoda_name = create_yoda_hist_name(plot_dirname, ang.var, (start_val, end_val))
+            rebin_hist = hist.Rebin(len(this_rebins)-1, yoda_name, array('d', this_rebins))
+            # Normalise
+            if rebin_hist.Integral() > 0:
+                rebin_hist.Scale(1./rebin_hist.Integral())
+            # Divide bin contents by bin width
+            # for bin_ind in range(1, rebin_hist.GetNbinsX()+1):
+            #     val = rebin_hist.GetBinContent(bin_ind)
+            #     width = rebin_hist.GetBinWidth(bin_ind)
+            #     rebin_hist.SetBinContent(bin_ind, val/width)
+            
             out_f.WriteTObject(rebin_hist)  # saves faffing with cd()
 
+            # create yoda plot info
+            xlabel = "%s $(%s)$" % (ang.name, ang.lambda_str.replace("#", "\\"))
+            title_str = "$%d < p_{T}^{jet} < %d$ GeV" % (start_val, end_val)
+            this_plot_info = YODA_PLOT_TEMPLATE.format(plotname=yoda_name, xlabel=xlabel, title=title_str, ylabel="p.d.f")
+            plot_file.write(this_plot_info)
+
+    # Add in pt hist
+    h_pt = cu.get_from_tfile(in_f, "Dijet_tighter/pt_jet_response_binning")
+    pt_name = "d01-x20-y01"
+    h_pt.SetName(pt_name)
+    this_plot_info = YODA_PLOT_TEMPLATE.format(plotname=pt_name, xlabel="$\langle p_{T}^{j}\\rangle$ GeV", title="", ylabel="N")
+    plot_file.write(this_plot_info)
+    out_f.WriteTObject(h_pt)
+
     out_f.Close()
+    plot_file.close()
 
 
 if __name__ == "__main__":
