5769c9f9119c2f0d850a726294bba7c2c0cd43f8
Robin Aggleton <robin.aggleton@cern.ch>
2020-02-02 11:39:49 +0100

    Add RMS plots to summary plots

--- do_summary_unfolding_plots.py
+++ do_summary_unfolding_plots.py
@@ -83,7 +83,7 @@ class SummaryPlotter(object):
         dijet_central_data = df[mask & (df['region'] == region_name)]
         dijet_central_hist = self.data_to_hist(dijet_central_data['mean'], dijet_central_data['mean_err'], self.pt_bins_dijet)
         dijet_central_hist_truth = self.data_to_hist(dijet_central_data['mean_truth'], dijet_central_data['mean_err_truth'], self.pt_bins_dijet)
-        print(dijet_central_data)
+
         region_name = 'Dijet_forward'
         if do_groomed:
             region_name += "_groomed"
@@ -117,6 +117,7 @@ class SummaryPlotter(object):
                          marker_color=zpj_col, marker_style=cu.Marker.get('triangleUp', True), marker_size=m_size,
                          subplot=zpj_hist_truth),
 
+            # TODO: make truth plotting optional, also plot alternate generators
             Contribution(dijet_central_hist_truth, label='Dijet (central) [truth]',
                          line_color=dijet_cen_col, line_width=2,
                          marker_color=dijet_cen_col, marker_style=cu.Marker.get('circle', False), marker_size=m_size),
@@ -165,6 +166,109 @@ class SummaryPlotter(object):
         groomed_str = '_groomed' if do_groomed else ''
         plot.save("%s/dijet_zpj_means_vs_pt_%s%s_%s.%s" % (output_dir, angle.var, groomed_str, jet_algo['name'], self.output_fmt))
 
+    def plot_dijet_zpj_rms_vs_pt_all(self):
+        """Plot mean vs pt for dijet (cen+fwd) & Z+jet on one plot,
+        per angle, per jet algo, per groomed/ungroomed"""
+        print('plot_dijet_zpj_rms_vs_pt_all...')
+        for jet_algo, angle, groomed in product(self.jet_algos[:1], self.angles[:], [False, True][:]):
+            print("  ...doing", jet_algo['label'], angle.name, 'groomed' if groomed else 'ungroomed')
+            self.plot_dijet_zpj_rms_vs_pt_one_angle_one_jet(angle, jet_algo, do_groomed=groomed, output_dir='%s/plot_dijet_zpj_rms_vs_pt_all' % self.output_dir)
+
+    def plot_dijet_zpj_rms_vs_pt_one_angle_one_jet(self, angle, jet_algo, do_groomed, output_dir):
+        """Do plot of RMS lambda vs pt, for the dijet cen+fwd and zpj regions, for a given angle/jet algo/grooming"""
+        df = self.df
+        mask = ((df['angle'] == angle.var) & (df['jet_algo'] == jet_algo['name']) & (df['isgroomed'] == do_groomed))
+
+        region_name = 'Dijet_central'
+        if do_groomed:
+            region_name += "_groomed"
+        dijet_central_data = df[mask & (df['region'] == region_name)]
+        dijet_central_hist = self.data_to_hist(dijet_central_data['rms'], dijet_central_data['rms_err'], self.pt_bins_dijet)
+        dijet_central_hist_truth = self.data_to_hist(dijet_central_data['rms_truth'], dijet_central_data['rms_err_truth'], self.pt_bins_dijet)
+
+        region_name = 'Dijet_forward'
+        if do_groomed:
+            region_name += "_groomed"
+        dijet_forward_data = df[mask & (df['region'] == region_name)]
+        dijet_forward_hist = self.data_to_hist(dijet_forward_data['rms'], dijet_forward_data['rms_err'], self.pt_bins_dijet)
+        dijet_forward_hist_truth = self.data_to_hist(dijet_forward_data['rms_truth'], dijet_forward_data['rms_err_truth'], self.pt_bins_dijet)
+
+        region_name = 'ZPlusJets'
+        if do_groomed:
+            region_name += "_groomed"
+        zpj_data = df[mask & (df['region'] == region_name)]
+        zpj_hist = self.data_to_hist(zpj_data['rms'], zpj_data['rms_err'], self.pt_bins_zpj)
+        zpj_hist_truth = self.data_to_hist(zpj_data['rms_truth'], zpj_data['rms_err_truth'], self.pt_bins_zpj)
+
+        # unify this?
+        dijet_cen_col = ROOT.kBlack
+        dijet_fwd_col = ROOT.kRed
+        zpj_col = ROOT.kBlue
+        m_size = 1
+        entries = [
+            Contribution(dijet_central_hist, label='Dijet (central)',
+                         line_color=dijet_cen_col, line_width=2,
+                         marker_color=dijet_cen_col, marker_style=cu.Marker.get('circle', True), marker_size=m_size,
+                         subplot=dijet_central_hist_truth),
+            Contribution(dijet_forward_hist, label='Dijet (forward)',
+                         line_color=dijet_fwd_col, line_width=2,
+                         marker_color=dijet_fwd_col, marker_style=cu.Marker.get('square', True), marker_size=m_size,
+                         subplot=dijet_forward_hist_truth),
+            Contribution(zpj_hist, label='Z+jet',
+                         line_color=zpj_col, line_width=2,
+                         marker_color=zpj_col, marker_style=cu.Marker.get('triangleUp', True), marker_size=m_size,
+                         subplot=zpj_hist_truth),
+
+            # TODO: make truth plotting optional, also plot alternate generators
+            Contribution(dijet_central_hist_truth, label='Dijet (central) [truth]',
+                         line_color=dijet_cen_col, line_width=2,
+                         marker_color=dijet_cen_col, marker_style=cu.Marker.get('circle', False), marker_size=m_size),
+            Contribution(dijet_forward_hist_truth, label='Dijet (forward) [truth]',
+                         line_color=dijet_fwd_col, line_width=2,
+                         marker_color=dijet_fwd_col, marker_style=cu.Marker.get('square', False), marker_size=m_size),
+            Contribution(zpj_hist_truth, label='Z+jet [truth]',
+                         line_color=zpj_col, line_width=2,
+                         marker_color=zpj_col, marker_style=cu.Marker.get('triangleUp', False), marker_size=m_size),
+        ]
+
+        angle_prepend = "Groomed " if do_groomed else ""
+        this_angle_name = angle.name
+        if (angle_prepend != ""
+            and 'LHA' not in this_angle_name
+            and "_{T}" not in this_angle_name
+            and "PUPPI" not in this_angle_name):
+            # lower case if Groomed..., but be careful of e.g. pTD, LHA
+            this_angle_name = this_angle_name[0].lower() + this_angle_name[1:]
+        # for plot axis titles
+        angle_str = "RMS {prepend}{name}  {lambda_str}".format(prepend=angle_prepend,
+                                                             name=this_angle_name,
+                                                             lambda_str=angle.lambda_str)
+        h_max = max([c.obj.GetMaximum() for c in entries])
+        plot = Plot(entries,
+                    what='hist',
+                    xtitle='Jet p_{T} [GeV]',
+                    ytitle=angle_str,
+                    title="%s jets" % (jet_algo['label']),
+                    # ylim=(0, h_max*1.75),
+                    has_data=self.has_data,
+                    subplot_type='ratio',
+                    subplot_title='Unfolded / truth',
+                    subplot_limits=(0.9, 1.1))
+        plot.title_start_y = 0.85
+        plot.title_left_offset = 0.05
+        plot.title_font_size = 0.035
+        plot.legend.SetX1(0.6)
+        plot.legend.SetY1(0.7)
+        plot.legend.SetY2(0.85+0.02)
+        plot.left_margin = 0.16
+        plot.y_padding_min_linear = 0.5
+        # plot.default_canvas_size = (600, 800)
+        plot.plot("NOSTACK E1")
+        # plot.get_modifier().GetYaxis().SetTitleOffset(plot.get_modifier().GetYaxis().GetTitleOffset()*1.1)
+        plot.set_logx(do_more_labels=False)
+        groomed_str = '_groomed' if do_groomed else ''
+        plot.save("%s/dijet_zpj_rms_vs_pt_%s%s_%s.%s" % (output_dir, angle.var, groomed_str, jet_algo['name'], self.output_fmt))
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description=__doc__)
@@ -177,7 +281,7 @@ if __name__ == "__main__":
                         help='Output directory (default is the source dir')
     args = parser.parse_args()
     if not args.outputDir:
-        args.outputDir = args.ak4source
+        args.outputDir = os.path.join(args.ak4source, 'SummaryPlots')
 
     results_dicts = []
 
@@ -226,12 +330,13 @@ if __name__ == "__main__":
                 alt_unfolder = unpack_dict['alt_unfolder']
                 alt_hist_truth = unpack_dict['alt_hist_truth']
 
-                # Big 1D plots to compare things
                 hbc = dup.HistBinChopper(unfolder)
                 hbc.add_obj("unfolded", unfolder.unfolded)
                 hbc.add_obj("unfolded_stat_err", unfolder.unfolded_stat_err)
                 hbc.add_obj("hist_truth", unfolder.hist_truth)
 
+                # Iterate through pt bins, get lambda histogram for that bin,
+                # derive metrics from it, save
                 key = 'signal_gen'
                 if 'ZPlusJets' in region['name']:
                     key = 'signal_zpj_gen'
@@ -254,6 +359,8 @@ if __name__ == "__main__":
                         'mean_err_truth': mc_gen_hist_bin.GetMeanError(),
                         'rms': unfolded_hist_bin_total_errors.GetRMS(),
                         'rms_err': unfolded_hist_bin_total_errors.GetRMSError(),
+                        'rms_truth': mc_gen_hist_bin.GetRMS(),
+                        'rms_err_truth': mc_gen_hist_bin.GetRMSError(),
                     }
                     results_dicts.append(result_dict)
 
@@ -267,3 +374,4 @@ if __name__ == "__main__":
 
     plotter = SummaryPlotter(jet_algos, regions, angles, qgc.PT_UNFOLD_DICT['signal_gen'], qgc.PT_UNFOLD_DICT['signal_zpj_gen'], df, args.outputDir)
     plotter.plot_dijet_zpj_means_vs_pt_all()
+    plotter.plot_dijet_zpj_rms_vs_pt_all()
