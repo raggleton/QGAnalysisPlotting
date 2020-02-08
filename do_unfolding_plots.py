fff4c1d198e909ccaa88c882021d9291252f25b4
Robin Aggleton <robin.aggleton@cern.ch>
2020-02-08 16:37:19 +0100

    Add plot of experimental syst variations / nominal value like in Top-17-013 AN

--- do_unfolding_plots.py
+++ do_unfolding_plots.py
@@ -270,6 +270,40 @@ PLOT_COLOURS = dict(
     reco_folded_mc_truth_colour=ROOT.kGreen+2,
 )
 
+def calc_auto_xlim(entries):
+    """Figure out x axis range that includes all non-0 bins from all Contributions"""
+    if len(entries) == 0:
+        return None
+    if not isinstance(entries[0], Contribution):
+        raise TypeError("calc_auto_xlim: `entries` should be a list of Contributions")
+
+    x_min = 9999999999
+    x_max = -9999999999
+    for ent in entries:
+        obj = ent.obj
+        if not isinstance(obj, ROOT.TH1):
+            raise TypeError("Cannot handle obj in calc_auto_xlim")
+        xax = obj.GetXaxis()
+        nbins = obj.GetNbinsX()
+        found_min = False
+        for i in range(1, nbins+1):
+            val = obj.GetBinContent(i)
+            if not found_min:
+                # find the first non-empty bin
+                if val != 0:
+                    x_min = min(xax.GetBinLowEdge(i), x_min)
+                    found_min = True
+            else:
+                # find the first empty bin
+                if val == 0:
+                    x_max = max(xax.GetBinLowEdge(i+1), x_max)
+                    break
+    if x_max > x_min:
+        return (x_min, x_max)
+    else:
+        # default x max
+        return None
+
 # FIXME: generalise this and LambdaBinnedPlotter into one generic BinnedPlotter?
 # Although each has different set of plots, so not easy/possible
 class GenPtBinnedPlotter(object):
@@ -844,6 +878,108 @@ class GenPtBinnedPlotter(object):
             # plot.subplot_limits = (0.9, 1.1)
             plot.plot("NOSTACK E1")
             plot.save("%s/unfolded_syst_variations_%s_bin_%d_divBinWidth.%s" % (self.setup.output_dir, self.setup.append, ibin, self.setup.output_fmt))
+
+    def plot_exp_syst_variation_normalised(self, unfolder):
+        """Plot varation / central value on normalised hists
+        (basically the subplot from plot_unfolded_with_exp_systs_normalised)
+        """
+        def _convert_error_bars_to_error_ratio_hist(h, direction=1):
+            h_new = h.Clone(h.GetName() + cu.get_unique_str())
+            for ibin in range(1, h_new.GetNbinsX()+1):
+                if h.GetBinContent(ibin) > 0:
+                    h_new.SetBinContent(ibin, 1+(direction*(h.GetBinError(ibin) / h.GetBinContent(ibin))))
+                else:
+                    h_new.SetBinContent(ibin, 0)
+                h_new.SetBinError(ibin, 0)
+            return h_new
+
+        def _convert_syst_shift_to_error_ratio_hist(h_syst, h_nominal):
+            h_new = h_syst.Clone(h_syst.GetName() + cu.get_unique_str())
+            h_new.Divide(h_nominal)
+            for ibin in range(1, h_new.GetNbinsX()+1):
+                h_new.SetBinError(ibin, 0)
+            return h_new
+
+        for ibin, (bin_edge_low, bin_edge_high) in enumerate(zip(self.bins[:-1], self.bins[1:])):
+            # Get total for this bin
+            unfolded_hist_bin_total_errors = self.hist_bin_chopper.get_pt_bin_normed_div_bin_width('unfolded', ibin, binning_scheme='generator')
+            # Get stat err from input for this bin
+            unfolded_hist_bin_stat_errors = self.hist_bin_chopper.get_pt_bin_normed_div_bin_width('unfolded_stat_err', ibin, binning_scheme='generator')
+            # Get stat err from response matrix for this bin
+            unfolded_hist_bin_rsp_errors = self.hist_bin_chopper.get_pt_bin_normed_div_bin_width('unfolded_syst_err', ibin, binning_scheme='generator')
+
+            entries = []
+            tmp = []
+            for syst_dict, mark in zip(self.region['experimental_systematics'], cu.Marker().cycle(cycle_filling=True)):
+                syst_label_no_spaces = cu.no_space_str(syst_dict['label'])
+                self.hist_bin_chopper.add_obj('syst_shifted_%s_unfolded' % syst_label_no_spaces, unfolder.systs_shifted[syst_dict['label']])
+                syst_unfolded_hist_bin = self.hist_bin_chopper.get_pt_bin_normed_div_bin_width('syst_shifted_%s_unfolded' % (syst_label_no_spaces), ibin, binning_scheme='generator')
+                this_syst_hist = _convert_syst_shift_to_error_ratio_hist(syst_unfolded_hist_bin, unfolded_hist_bin_total_errors)
+                tmp.append(this_syst_hist)
+                c = Contribution(this_syst_hist,
+                                 label=syst_dict['label'],
+                                 line_color=syst_dict['colour'], 
+                                 line_width=0,
+                                 # line_width=self.line_width,
+                                 # line_style=2 if 'down' in syst_dict['label'].lower() else 1,
+                                 marker_color=syst_dict['colour'], marker_size=1.25,
+                                 marker_style=mark)
+                entries.append(c)
+
+            entries.extend([
+                Contribution(_convert_error_bars_to_error_ratio_hist(unfolded_hist_bin_total_errors),
+                             label="Total uncertainty",
+                             line_color=self.plot_colours['unfolded_total_colour'], line_width=self.line_width, line_style=2,
+                             marker_color=self.plot_colours['unfolded_total_colour'], marker_style=20, marker_size=0,
+                             fill_style=0, fill_color=15),
+                # Add in the -ve side, but no label as we don't want it in the legend
+                Contribution(_convert_error_bars_to_error_ratio_hist(unfolded_hist_bin_total_errors, -1),
+                             line_color=self.plot_colours['unfolded_total_colour'], line_width=self.line_width, line_style=2,
+                             marker_color=self.plot_colours['unfolded_total_colour'], marker_style=20, marker_size=0,
+                             fill_style=0, fill_color=15),
+                Contribution(_convert_error_bars_to_error_ratio_hist(unfolded_hist_bin_stat_errors),
+                             label="Input stats",
+                             line_color=ROOT.kRed, line_width=self.line_width, line_style=3,
+                             marker_color=ROOT.kRed, marker_style=20, marker_size=0,
+                             fill_style=0, fill_color=13),
+                # Add in the -ve side, but no label as we don't want it in the legend
+                Contribution(_convert_error_bars_to_error_ratio_hist(unfolded_hist_bin_stat_errors, -1),
+                             line_color=ROOT.kRed, line_width=self.line_width, line_style=3,
+                             marker_color=ROOT.kRed, marker_style=20, marker_size=0,
+                             fill_style=0, fill_color=13),
+                Contribution(_convert_error_bars_to_error_ratio_hist(unfolded_hist_bin_rsp_errors),
+                             label="Response matrix stats",
+                             line_color=ROOT.kGray+2, line_width=self.line_width, line_style=3,
+                             marker_color=ROOT.kGray+2, marker_style=20, marker_size=0,
+                             fill_style=0, fill_color=13),
+                # Add in the -ve side, but no label as we don't want it in the legend
+                Contribution(_convert_error_bars_to_error_ratio_hist(unfolded_hist_bin_rsp_errors, -1),
+                             line_color=ROOT.kGray+2, line_width=self.line_width, line_style=3,
+                             marker_color=ROOT.kGray+2, marker_style=20, marker_size=0,
+                             fill_style=0, fill_color=13),
+            ])
+
+            if not self.check_entries(entries, "plot_exp_syst_variation_normalised %d" % ibin):
+                return
+            plot = Plot(entries,
+                        xtitle=self.setup.particle_title,
+                        ytitle='Variation / nominal (on normalised distribution)',
+                        what="hist",
+                        title=self.get_pt_bin_title(bin_edge_low, bin_edge_high),
+                        has_data=self.setup.has_data,
+                        ylim=(0.7, 1.5),
+                        xlim=calc_auto_xlim(entries),
+                        subplot_type=None)
+            self._modify_plot(plot)
+            plot.default_canvas_size = (800, 700)
+            plot.legend.SetX1(0.5)
+            plot.legend.SetY1(0.65)
+            plot.legend.SetX2(0.93)
+            plot.legend.SetY2(0.88)
+            if len(entries) > 5: plot.legend.SetNColumns(2)
+            plot.plot("NOSTACK P L") # hard to get one that is points for systs, and line for stats
+            plot.save("%s/unfolded_syst_variations_vs_nominal_%s_bin_%d_divBinWidth.%s" % (self.setup.output_dir, self.setup.append, ibin, self.setup.output_fmt))
+
 
     def plot_detector_normalised(self, unfolder, alt_detector=None):
         for ibin, (bin_edge_low, bin_edge_high) in enumerate(zip(self.bins[:-1], self.bins[1:])):
@@ -1665,6 +1801,7 @@ def do_all_plots_per_region_angle(setup, unpack_dict):
     if has_exp_systs:
         gen_pt_binned_plotter.plot_uncertainty_shifts_normalised(unfolder=unfolder)
         gen_pt_binned_plotter.plot_unfolded_with_exp_systs_normalised(unfolder=unfolder)
+        gen_pt_binned_plotter.plot_exp_syst_variation_normalised(unfolder=unfolder)
 
     if has_model_systs:
         gen_pt_binned_plotter.plot_unfolded_with_model_systs_normalised(unfolder=unfolder)
