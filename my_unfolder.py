a8b9558662931f5084977c388d2126cd4ed397b9
Robin Aggleton <robin.aggleton@cern.ch>
2020-08-24 12:43:46 +0100

    Add condition number calculation without underflow

--- my_unfolder.py
+++ my_unfolder.py
@@ -1395,12 +1395,13 @@ class MyUnfolder(ROOT.MyTUnfoldDensity):
         return self.vyy_ndarray
 
     @staticmethod
-    def calculate_singular_max_min(matrix):
+    def calculate_singular_max_min(matrix, non_zero=False):
         """Calculate max & min singular values condition number as per StatsComm guidelines
 
         These are found using TDecompSVD.
         (we ignore the builtin condition() method as it calculates it differently)
         """
+        print("Calculating singular values on matrix size (nrows, ncols): (%d, %d)" % (matrix.GetNrows(), matrix.GetNcols()))
         if matrix.GetNcols() > matrix.GetNrows():
             raise RuntimeError("Condition number only for matrix where # rows >= # cols")
 
@@ -1411,6 +1412,8 @@ class MyUnfolder(ROOT.MyTUnfoldDensity):
         print("sigma_max:", sigma_max, "sigma_min:", sigma_min)
         if sigma_min == 0:
             print("sigma_min > 0:", min([x for x in sig if x>0]))
+            if non_zero:
+                sigma_min = min([x for x in sig if x>0])
         return sigma_max, sigma_min
 
     def print_condition_number(self):
@@ -1421,7 +1424,7 @@ class MyUnfolder(ROOT.MyTUnfoldDensity):
         These are also stored for later usage if needed (since expensive to calc)
         """
         if getattr(self, 'condition_number', None) is None:
-            sigma_max, sigma_min = self.calculate_singular_max_min(cu.th2_to_tmatrixd(self.get_probability_matrix()))
+            sigma_max, sigma_min = self.calculate_singular_max_min(cu.th2_to_tmatrixd(self.get_probability_matrix()), non_zero=True)
             if sigma_min == 0:
                 # avoid DivisionError
                 print("Minmum singular value = 0, condition number = Infinity")
@@ -1439,6 +1442,33 @@ class MyUnfolder(ROOT.MyTUnfoldDensity):
             print(" - You probably should regularize this")
         else:
             print(" - You probably should look into regularization")
+
+        # try a version cutting out underflow bins
+        start_ind_gen = self.generator_distribution.GetStartBin() - 1 # -1 as numpy starts at 0, TUnfold at 1
+        start_ind_det = self.detector_distribution.GetStartBin() - 1 # -1 as numpy starts at 0, TUnfold at 1
+        print("Signal Start ind gen:", start_ind_gen)
+        print("Signal Start ind det:", start_ind_det)
+        print("prob ndarray.shape:", self.probability_ndarray.shape)
+        probability_ndarray_signal_pt = np.copy(self.probability_ndarray)[start_ind_det:, start_ind_gen:]
+        sigma_max, sigma_min = self.calculate_singular_max_min(cu.th2_to_tmatrixd(cu.ndarray_to_th2(probability_ndarray_signal_pt)), non_zero=True)
+        if sigma_min == 0:
+            # avoid DivisionError
+            print("Minmum singular value = 0, condition number = Infinity")
+            num = np.inf
+        else:
+            num = sigma_max / sigma_min
+        print("Condition number without pt < 50 bins:", num)
+
+        # now also do a version removing the overflow bins in each variable & pT:
+        # probability_ndarray_no_oflow = self.get_ndarray_signal_region_no_overflow(self.get_probability_matrix(), xbinning='generator', ybinning='detector')
+        # sigma_max, sigma_min = self.calculate_singular_max_min(cu.th2_to_tmatrixd(cu.ndarray_to_th2(probability_ndarray_no_oflow)), non_zero=True)
+        # if sigma_min == 0:
+        #     # avoid DivisionError
+        #     print("Minmum singular value = 0, condition number = Infinity")
+        #     num = np.inf
+        # else:
+        #     num = sigma_max / sigma_min
+        # print("Condition number without pt < 50 bins and without overflow bins:", num)
 
     def get_response_normed_by_detector_pt(self):
         if getattr(self, 'response_map_normed_by_detector_pt', None) is None:
