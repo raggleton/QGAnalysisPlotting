bd3489843663bba5e95c3b4c9791309aa9ff150c
Robin Aggleton <robin.aggleton@cern.ch>
2020-12-16 13:34:06 +0000

    Add more functions to to MyTUnfoldDensity, split out checkinput part

--- MyTUnfoldDensity.cpp
+++ MyTUnfoldDensity.cpp
@@ -7,7 +7,7 @@
 #ifndef My_TUnfoldDensity
 #define My_TUnfoldDensity
 
-#include "TUnfoldDensity.h"
+// #include "TUnfoldDensity.h"
 
 class MyTUnfoldDensity : public TUnfoldDensity {
 public:
@@ -38,6 +38,7 @@ public:
 
     // Here are methods that were protected in TUnfoldDensity, that are now public for us to use
  
+    virtual TString GetOutputBinName(Int_t iBinX) const { return TUnfold::GetOutputBinName(iBinX); } // name a bin
     // multiply sparse and non-sparse matrix
     TMatrixDSparse *MultiplyMSparseM(const TMatrixDSparse *a,const TMatrixD *b) const { return TUnfold::MultiplyMSparseM(a, b); } 
     // multiply sparse and sparse matrix
@@ -59,7 +60,16 @@ public:
     void AddMSparse(TMatrixDSparse *dest,Double_t f,const TMatrixDSparse *src) const { return TUnfold::AddMSparse(dest, f, src); } 
     // create a TMatrixDSparse from an array
     TMatrixDSparse *CreateSparseMatrix(Int_t nrow,Int_t ncol,Int_t nele,Int_t *row,Int_t *col,Double_t *data) const { return TUnfold::CreateSparseMatrix(nrow, ncol, nele, row, col, data); } 
+    TMatrixDSparse *CreateSparseMatrix(Int_t nrow,Int_t ncol,Int_t nele,std::vector<Int_t> row,std::vector<Int_t> col,std::vector<Double_t> data) const { return TUnfold::CreateSparseMatrix(nrow, ncol, nele, &row[0], &col[0], &data[0]); } 
 
+    /// returns internal number of output (truth) matrix rows
+    inline Int_t GetNx(void) const { return TUnfold::GetNx(); }
+    /// converts truth histogram bin number to matrix row
+    inline Int_t GetRowFromBin(int ix) const { return TUnfold::GetRowFromBin(ix); }
+    /// converts matrix row to truth histogram bin number
+    inline Int_t GetBinFromRow(int ix) const { return TUnfold::GetBinFromRow(ix); }
+    /// returns the number of measurement bins
+    inline Int_t GetNy(void) const { return TUnfold::GetNy(); }
     /// vector of the unfolding result
     inline const TMatrixD *GetX(void) const { return TUnfoldDensity::GetX(); }
     /// covariance matrix of the result
@@ -80,8 +90,6 @@ public:
     inline const TMatrixDSparse *GetE(void) const { return TUnfoldDensity::GetE(); }
     /// vector of the input data y (background-subtracted)
     inline const TMatrixD *GetY(void) const { return fY; }
-    /// returns the number of measurement bins
-    inline const Int_t GetNy(void) const { return TUnfoldDensity::GetNy(); }
     /// covariance matrix of the data y
     inline const TMatrixDSparse *GetVyy(void) const { return fVyy; }
     /// inverse of covariance matrix of the data y
@@ -96,6 +104,10 @@ public:
    // return an error matrix as histogram
     void ErrorMatrixToHist(TH2 *ematrix, const TMatrixDSparse *emat) const { TUnfold::ErrorMatrixToHist(ematrix, emat, nullptr, true); }
 
+    /// delete matrix and invalidate pointer
+    static void DeleteMatrix(TMatrixD **m) { TUnfold::DeleteMatrix(m); }
+    /// delete sparse matrix and invalidate pointer
+    static void DeleteMatrix(TMatrixDSparse **m) { TUnfold::DeleteMatrix(m); }
 
     /// determine total error matrix on the vector Ax
     TMatrixDSparse *GetSummedErrorMatrixYY(void) { return TUnfoldSys::GetSummedErrorMatrixYY(); }
@@ -105,6 +117,172 @@ public:
 
     Bool_t AddRegularisationCondition(Int_t i0, Double_t f0, Int_t i1=-1, Double_t f1=0., Int_t i2=-1, Double_t f2=0.) { return TUnfold::AddRegularisationCondition(i0,f0,i1,f1,i2,f2); } // add regularisation condition for a triplet of output bins
     Bool_t AddRegularisationCondition(Int_t nEle, const Int_t *indices, const Double_t *rowData) { return TUnfold::AddRegularisationCondition(nEle, indices, rowData); } // add a regularisation condition
+
+    // FIXME: this shouldn't need an arg?
+    vector<Int_t> CheckInput(const TH1 *hist_y=nullptr)
+    {
+        vector<Int_t> ignoredInputBins = {};
+
+        Double_t oneOverZeroError = 0.0;
+
+        Int_t *rowVyy1=new Int_t[GetNy()];
+        Int_t *colVyy1=new Int_t[GetNy()];
+        Double_t *dataVyy1=new Double_t[GetNy()];
+        Double_t *dataVyyDiag=new Double_t[GetNy()];
+
+        Int_t nVarianceZero=0;
+        Int_t nVyy1=0;
+        if (hist_y == nullptr && fVyy == nullptr) {
+            throw std::runtime_error("hist_y and fVyy are empty, either specify hist_y or call SetInput() first");
+        }
+        for (Int_t iy = 0; iy < GetNy(); iy++) {
+            // diagonals
+            Double_t dy;
+            if (hist_y != nullptr)
+                dy = hist_y->GetBinError(iy + 1);
+            else
+                dy = (*fVyy)(iy, iy);
+            Double_t dy2=dy*dy;
+            if (dy2 <= 0.0) {
+                nVarianceZero++;
+            }
+            rowVyy1[nVyy1] = iy;
+            colVyy1[nVyy1] = 0;
+            dataVyyDiag[iy] = dy2;
+            if(dy2>0.0) {
+                dataVyy1[nVyy1++] = dy2;
+            }
+        }
+
+        TMatrixDSparse *vecV=CreateSparseMatrix(GetNy(),1,nVyy1,rowVyy1,colVyy1, dataVyy1);
+
+        delete[] rowVyy1;
+        delete[] colVyy1;
+        delete[] dataVyy1;
+
+        // simple check whether unfolding is possible, given the matrices fA and fV
+        TMatrixDSparse *mAtV=MultiplyMSparseTranspMSparse(fA,vecV);
+        DeleteMatrix(&vecV);
+        Int_t nError2=0;
+        //mAtV is a nx1 matrix
+        for (Int_t i = 0; i<mAtV->GetNrows();i++) {
+            if(mAtV->GetRowIndexArray()[i]==mAtV->GetRowIndexArray()[i+1]) {
+                nError2 ++;
+            }
+        }
+        if(nVarianceZero) {
+            if(nVarianceZero>1) {
+                Warning("CheckInput","%d/%d input bins have zero error,"
+                    " and are ignored.",nVarianceZero,GetNy());
+            } else {
+                Warning("CheckInput","One input bin has zero error,"
+                    " and is ignored.");
+            }
+        }
+        // printout mAtV vector
+        for (Int_t irow = 0; irow < mAtV->GetNrows(); irow++) {
+          const Int_t sIndex = mAtV->GetRowIndexArray()[irow];
+          const Int_t eIndex = mAtV->GetRowIndexArray()[irow+1];
+          if (sIndex == eIndex) {
+            cout << "sIndex==eIndex for sIndex= " << sIndex << endl;
+          }
+          for (Int_t index = sIndex; index < eIndex; index++) {
+            const Int_t icol = mAtV->GetColIndexArray()[index];
+            const Double_t data = mAtV->GetMatrixArray()[index];
+            // printf("mAtV(%d,%d) = %.4e\n",irow,icol,data);
+          }
+        }
+
+        if(nError2>0) {
+            // check whether data points with zero error are responsible
+            if(oneOverZeroError<=0.0) {
+                // a_rows[i] has the start index (in a_cols, a_data) 
+                // for row i, a_rows[i+1] is the end index + 1
+                // a_cols[a_rows[i]]...a_cols[a_rows[i+1]-1] 
+                // is the set of column indices that have !=0 data
+                // a_data[a_rows[i]]...a_data[a_rows[i+1]-1] 
+                // is the set of corresponding data
+                cout << "fA NRows: " << fA->GetNrows() << endl;
+                TMatrixDSparse *aT = new TMatrixDSparse(fA->GetNcols(), fA->GetNrows());
+                aT->Transpose(*fA);
+                cout << "aT NRows: " << aT->GetNrows() << endl;
+
+                const Int_t *a_rows = aT->GetRowIndexArray(); 
+                const Int_t *a_cols = aT->GetColIndexArray();
+                const Double_t *a_data = aT->GetMatrixArray();
+
+                // for each row in the resultant mAtV, look for 0 entries.
+                // for each of those, look at all the A^T and dataVyyDiag pairs
+                // (i.e. iterate over columns) and see which are 0
+                for (Int_t row = 0; row <mAtV->GetNrows();row++) {
+                    // printout every row's sum entries
+                    // printf("++++ ROW %d\n", row);
+                    // const Int_t sIndex = a_rows[row];
+                    // const Int_t eIndex = a_rows[row+1];
+                    // for (Int_t i=sIndex; i<eIndex; i++) {
+                    //     Int_t col = a_cols[i];
+                    //     cout << "dataVyyDiag[" << col << "]: " << dataVyyDiag[col] << endl;
+                    //     printf("a^T(%d,%d) = %.4e\n", row, col, a_data[i]);
+                    // }
+
+                    if (mAtV->GetRowIndexArray()[row] == mAtV->GetRowIndexArray()[row+1]) {
+                        TString binlist("no data to constrain output bin ");
+                        binlist += GetOutputBinName(fXToHist[row]);
+                        binlist +=" depends on ignored input bins ";
+
+                        const Int_t sIndex = a_rows[row];
+                        const Int_t eIndex = a_rows[row+1];
+                        for (Int_t i=sIndex; i<eIndex; i++) {
+                            Int_t col = a_cols[i];
+                            if (dataVyyDiag[col] > 0.) continue;
+                            // cout << "dataVyyDiag[" << col << "]: " << dataVyyDiag[col] << endl;
+                            // printf("a^T(%d,%d) = %.4e\n", row, col, a_data[i]);
+                            binlist +=" ";
+                            binlist += col+1; // +1 for ROOT binning
+                            ignoredInputBins.push_back(col+1);
+                        }
+                        // inhereted from TObject
+                        Warning("CheckInput",binlist);
+                    }
+                }
+            }
+            
+            // TUnfold original version of the same thing that I can't understand :(
+            // just to check I did it right
+            if(oneOverZeroError<=0.0) {
+                const Int_t *a_rows=fA->GetRowIndexArray();
+                const Int_t *a_cols=fA->GetColIndexArray();
+                for (Int_t col = 0; col <mAtV->GetNrows();col++) {
+                    if(mAtV->GetRowIndexArray()[col]==mAtV->GetRowIndexArray()[col+1]) {
+                        TString binlist("no data to constrain output bin ");
+                        binlist += GetOutputBinName(fXToHist[col]);
+                        binlist +=" depends on ignored input bins ";
+                        for(Int_t row=0;row<fA->GetNrows();row++) {
+                            if(dataVyyDiag[row]>0.0) continue;
+                            for(Int_t i=a_rows[row];i<a_rows[row+1];i++) {
+                                if(a_cols[i]!=col) continue;
+                                binlist +=" ";
+                                binlist +=row+1;
+                            }
+                        }  
+                        Warning("SetInput", binlist);
+                    }
+                }
+            }
+
+            if(nError2>1) {
+                Error("CheckInput","%d/%d output bins are not constrained by any data.",
+                    nError2,mAtV->GetNrows());
+            } else {
+                Error("CheckInput","One output bins is not constrained by any data.");
+            }
+        }
+        DeleteMatrix(&mAtV);
+
+        delete[] dataVyyDiag;
+
+        return ignoredInputBins;
+    }
 
     ClassDef(MyTUnfoldDensity, 1) //My Unfolding with density regularisation
 
