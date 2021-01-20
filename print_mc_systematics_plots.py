bc1652e64c4fec1691d7fd61dfc9aa479c828278
Robin Aggleton <robin.aggleton@cern.ch>
2021-01-20 16:11:17 +0000

    use do_all_exclusive_plots_comparison() in mc_systematics plots, add DY

--- print_mc_systematics_plots.py
+++ print_mc_systematics_plots.py
@@ -14,7 +14,7 @@ from array import array
 from copy import deepcopy
 
 # My stuff
-from comparator import Contribution, Plot, grab_obj
+from comparator import Contribution, Plot
 import qg_common as qgc
 import qg_general_plots as qgp
 import common_utils as cu
@@ -47,196 +47,163 @@ def do_plots(nominal_dir, herwig_dir, output_dir,
     herwig_dir is the herwig directory for comparison
     syst_ups and syst_downs are lists of (dirname, label) to add to the plot
     """
+    lw = 2
 
-    # QG variable plots
-    pt_bins = qgc.PT_BINS
-    # pt_bins = qgc.THEORY_PT_BINS
-    var_list = qgc.COMMON_VARS
-    var_prepend = ""
+    # COMPARE QCD
+    if os.path.isfile(os.path.join(nominal_dir, qgc.QCD_FILENAME)):
+        sources = [
+           {
+               "root_dir": nominal_dir,
+               'label': "Nominal MG5+Pythia8",
+               "style": {
+                   'line_style': 1,
+                   'line_width': lw,
+                   'line_color': qgc.QCD_COLOUR,
+                   'fill_color': qgc.QCD_COLOUR,
+                   'marker_color': qgc.QCD_COLOUR,
+                   'marker_style': cu.Marker.get(qgc.QCD_MARKER),
+                   'marker_size': 0,
+               }
 
-    plot_dirname = "Dijet_QG_tighter"
-    # plot_dirname = "Dijet_QG"
-    plot_dirname = "ZPlusJets_QG"
+           }
+        ]
 
-    # setup TFiles here to cache things
-    main_filename = qgc.QCD_FILENAME
-    main_filename = qgc.DY_FILENAME
-    nominal_tfile = cu.TFileCacher(os.path.join(nominal_dir, main_filename))
-    herwig_filename = qgc.QCD_HERWIG_FILENAME
-    herwig_filename = qgc.DY_HERWIG_FILENAME
-    herwig_tfile = cu.TFileCacher(os.path.join(herwig_dir, herwig_filename)) if herwig_dir else None
-    mg_herwig_tfile = cu.TFileCacher(os.path.join(herwig_dir, qgc.DY_MG_HERWIG_FILENAME)) if herwig_dir else None
-    pythia_tfile = cu.TFileCacher(os.path.join(nominal_dir, qgc.QCD_PYTHIA_ONLY_FILENAME))
-    # data_tfile = cu.TFileCacher(os.path.join(nominal_dir, qgc.JETHT_ZB_FILENAME))
-    data_tfile = cu.TFileCacher(os.path.join(nominal_dir, qgc.SINGLE_MU_FILENAME))
+        # Add systematics
+        # SHIFT UP
+        if syst_ups:
+            for ind, (syst_up_dir, syst_up_label, syst_up_col) in enumerate(syst_ups):
+                this_source = deepcopy(sources[0])
+                this_source['root_dir'] = syst_up_dir
+                this_source.update(
+                    dict(style=dict(line_color=syst_up_col, line_width=lw, fill_color=syst_up_col, line_style=2,
+                                     marker_color=syst_up_col, marker_style=cu.Marker.get('triangleUp'), marker_size=0),
+                         label=syst_up_label,
+                         subplot=0)
+                )
+                sources.append(this_source)
 
-    syst_up_tfiles = [cu.TFileCacher(os.path.join(syst_dir, main_filename)) for (syst_dir, _, _) in syst_ups]
-    syst_down_tfiles = [cu.TFileCacher(os.path.join(syst_dir, main_filename)) for (syst_dir, _, _) in syst_downs]
+        # SHIFT DOWN
+        if syst_downs:
+            for ind, (syst_down_dir, syst_down_label, syst_down_col) in enumerate(syst_downs):
+                this_source = deepcopy(sources[0])
+                this_source['root_dir'] = syst_down_dir
+                this_source.update(
+                    dict(style=dict(line_color=syst_down_col, line_width=lw, fill_color=syst_down_col, line_style=3,
+                                    marker_color=syst_down_col, marker_style=cu.Marker.get('triangleDown'), marker_size=0),
+                         label=syst_down_label, subplot=0)
+                )
+                sources.append(this_source)
 
-    for ang in var_list:
-        if ang.var not in qgc.ANGLE_REBIN_DICT:
-            continue
+        subplot_lim = (0.75, 1.25)
 
-        v = "%s%s_vs_pt" % (var_prepend, ang.var)
+        # qgp.do_all_exclusive_plots_comparison(sources,
+        #                                       var_list=qgc.COMMON_VARS,
+        #                                       pt_bins=qgc.PT_BINS,
+        #                                       qcd_filename=qgc.QCD_FILENAME,
+        #                                       dj_cen_dirname="Dijet_QG_central_tighter",
+        #                                       dj_fwd_dirname=None,
+        #                                       zpj_dirname=None,
+        #                                       plot_dir=os.path.join(output_dir, "plots_qcd_compare_dijet_central"),
+        #                                       subplot_type="ratio",
+        #                                       subplot_title="Variation / nominal",
+        #                                       subplot_limits=subplot_lim,
+        #                                       do_flav_tagged=False,
+        #                                       has_data=False)
 
-        for pt_ind, (start_val, end_val) in enumerate(pt_bins[:]):
-            dijet_entries = []
-            # Get all plots
-            lw = 2
-
-            ####################
-            # DIJET REGION
-            ####################
-
-            # NOMINAL MC
-            h2d_qcd_mc = nominal_tfile.Get("%s/%s" % (plot_dirname, v))
-            qcd_kwargs_mc = dict(line_color=qgc.QCD_COLOUR, line_width=lw, fill_color=qgc.QCD_COLOUR,
-                                 marker_color=qgc.QCD_COLOUR, marker_style=cu.Marker.get(qgc.QCD_MARKER), marker_size=0,
-                                 label="MG5+Pythia8")
-            nominal_hist = qgp.get_projection_plot(h2d_qcd_mc, start_val, end_val)
-            dijet_entries.append((nominal_hist, qcd_kwargs_mc))
-
-            # HERWIG MC
-            if herwig_dir is not None:
-                h2d_herwig_qcd_mc = herwig_tfile.Get("%s/%s" % (plot_dirname, v))
-                qcd_herwig_kwargs_mc = dict(line_color=qgc.HERWIGPP_QCD_COLOUR, line_width=lw, fill_color=qgc.HERWIGPP_QCD_COLOUR,
-                                     marker_color=qgc.HERWIGPP_QCD_COLOUR, marker_style=cu.Marker.get('square'), marker_size=0,
-                                     label="Herwig++",
-                                     subplot=nominal_hist)
-                dijet_entries.append((qgp.get_projection_plot(h2d_herwig_qcd_mc, start_val, end_val), qcd_herwig_kwargs_mc))
-
-                # h2d_mg_herwig_qcd_mc = mg_herwig_tfile.Get("%s/%s" % (plot_dirname, v))
-                # colh = ROOT.kGreen+2
-                # qcd_mg_herwig_kwargs_mc = dict(line_color=colh, line_width=lw, fill_color=colh,
-                #                      marker_color=colh, marker_style=cu.Marker.get('square'), marker_size=0,
-                #                      label="MG+Herwig++",
-                #                      subplot=nominal_hist)
-                # dijet_entries.append((qgp.get_projection_plot(h2d_mg_herwig_qcd_mc, start_val, end_val), qcd_mg_herwig_kwargs_mc))
-
-            # h2d_pythia_qcd_mc = pythia_tfile.Get("%s/%s" % (plot_dirname, v))
-            # colp = ROOT.kRed
-            # qcd_pythia_kwargs_mc = dict(line_color=colp, line_width=lw, fill_color=colp,
-            #                      marker_color=colp, marker_style=cu.Marker.get('square'), marker_size=0,
-            #                      label="Pythia",
-            #                      subplot=nominal_hist)
-            # dijet_entries.append((qgp.get_projection_plot(h2d_pythia_qcd_mc, start_val, end_val), qcd_pythia_kwargs_mc))
-
-            # SHIFT UP
-            if syst_ups:
-                for ind, (syst_up_dir, syst_up_label, syst_up_col) in enumerate(syst_ups):
-                    h2d_qcd_mc2 = syst_up_tfiles[ind].Get("%s/%s" % (plot_dirname, v))
-                    qcd_kwargs_mc2 = dict(line_color=syst_up_col, line_width=lw, fill_color=syst_up_col, line_style=2,
-                                          marker_color=syst_up_col, marker_style=cu.Marker.get('triangleUp'), marker_size=0,
-                                          label=syst_up_label,
-                                          subplot=nominal_hist)
-                    dijet_entries.append((qgp.get_projection_plot(h2d_qcd_mc2, start_val, end_val), qcd_kwargs_mc2))
-
-            # SHIFT DOWN
-            if syst_downs:
-                for ind, (syst_down_dir, syst_down_label, syst_down_col) in enumerate(syst_downs):
-                    h2d_qcd_mc3 = syst_down_tfiles[ind].Get("%s/%s" % (plot_dirname, v))
-                    qcd_kwargs_mc3 = dict(line_color=syst_down_col, line_width=lw, fill_color=syst_down_col, line_style=3,
-                                          marker_color=syst_down_col, marker_style=cu.Marker.get('triangleDown'), marker_size=0,
-                                          label=syst_down_label,
-                                          subplot=nominal_hist)
-                    dijet_entries.append((qgp.get_projection_plot(h2d_qcd_mc3, start_val, end_val), qcd_kwargs_mc3))
-
-            # JETHT-ZB DATA
-            h2d_qcd_data = data_tfile.Get("%s/%s" % (plot_dirname, v))
-            qcd_kwargs_data = dict(line_color=ROOT.kBlack, line_width=lw, fill_color=ROOT.kBlack,
-                                   marker_color=ROOT.kBlack, marker_style=cu.Marker.get('circle'), marker_size=1,
-                                   label="Data", subplot=nominal_hist)
-            data_hist = qgp.get_projection_plot(h2d_qcd_data, start_val, end_val)
-            dijet_entries.append((data_hist, qcd_kwargs_data))
-
-            # rebin here only affects constant bin width plots
-            # other set of plots uses binning structure defined at the top
-            rebin = 5
-            v_lower = v.lower()
-            if "multiplicity" in v_lower:
-                rebin = 2
-            elif "flavour" in v_lower or "thrust" in v_lower:
-                rebin = 2
-            elif "ptd" in v_lower:
-                rebin = 5
-
-            xlim = None
-            if "width" in v_lower or "ptd" in v_lower:
-                xlim = [0, 1]
-            elif"thrust" in v_lower:
-                xlim = [0, 0.5]
-            elif "multiplicity" in v_lower and "ak4" in nominal_dir.lower():
-                xlim = [0, 100]
-                xlim = [0, 80]
-                xlim = [
-                    [0, 40],
-                    [0, 40],
-                    [0, 40],
-                    [0, 50],
-                    [0, 50],
-                    [0, 60],
-                    [0, 60],
-                    [0, 75],
-                    [0, 75],
-                    [0, 80],
-                    [0, 80],
-                    [0, 90],
-                    [0, 90],
-                    [0, 100],
-                    [0, 100]
-                ][pt_ind]
-
-            # if xlim:
-            #     xlim[1] = max(xlim[1], nominal_hist.GetBinLowEdge(nominal_hist.GetNbinsX()+1))
-
-            ylim = None
-            if "flavour" in v_lower:
-                ylim = (0, 1)
-            elif "lha" in v_lower:
-                ylim = (0, 5)
-                ylim = None
-
-            # plot_dir = os.path.join(root_dir, "plots_dy_vs_qcd_mc_vs_data")
-            radius, pus = cu.get_jet_config_from_dirname(nominal_dir)
-            jet_str = "AK%s PF %s" % (radius.upper(), pus.upper())
-            subplot_title = "Variation / nominal"
-            subplot_limits = (0.9, 1.1)
-
-            qgp.do_comparison_plot(dijet_entries,
-                                   "%s/%s/ptBinned/%s_pt%dto%d_dijet.%s" % (output_dir, plot_dirname, v, start_val, end_val, OUTPUT_FMT),
-                                   rebin=rebin,
-                                   draw_opt="NOSTACK HIST E",
-                                   title="%d < p_{T}^{jet} < %d GeV\n%s" % (start_val, end_val, jet_str),
-                                   xtitle=ang.name + " (" + ang.lambda_str + ")",
-                                   xlim=xlim, ylim=ylim,
-                                   subplot_type='ratio',
-                                   subplot_title=subplot_title,
-                                   subplot_limits=subplot_limits,
-                                   has_data=False)
-
-            # dijet_entries_rebin = []
-            # this_rebins = qgc.ANGLE_REBIN_DICT[ang.var]
-            # rebin_hist_norminal = None
-            # for hist, kwargs in dijet_entries:
-            #     rebin_hist = hist.Rebin(len(this_rebins)-1, hist.GetName()+"Rebin", array('d', this_rebins))
-            #     if not rebin_hist_norminal:
-            #         rebin_hist_norminal = rebin_hist
-            #     new_kwargs = deepcopy(kwargs)
-            #     if 'subplot' in new_kwargs:
-            #         new_kwargs['subplot'] = rebin_hist_norminal
-            #     dijet_entries_rebin.append((rebin_hist, new_kwargs))
-
-            # qgp.do_comparison_plot(dijet_entries_rebin,
-            #                        "%s/ptBinned/%s_pt%dto%d_dijet_rebin.%s" % (output_dir, v, start_val, end_val, OUTPUT_FMT),
-            #                        title="%d < p_{T}^{jet} < %d GeV\n%s" % (start_val, end_val, jet_str),
-            #                        xtitle=ang.name + " (" + ang.lambda_str + ")",
-            #                        xlim=None, ylim=ylim,
-            #                        subplot_type='ratio',
-            #                        subplot_title=subplot_title,
-            #                        subplot_limits=subplot_limits,
-            #                        has_data=False)
+        # qgp.do_all_exclusive_plots_comparison(sources,
+        #                                       var_list=qgc.COMMON_VARS,
+        #                                       pt_bins=qgc.PT_BINS,
+        #                                       qcd_filename=qgc.QCD_FILENAME,
+        #                                       dj_cen_dirname=None,
+        #                                       dj_fwd_dirname="Dijet_QG_forward_tighter",
+        #                                       zpj_dirname=None,
+        #                                       plot_dir=os.path.join(output_dir, "plots_qcd_compare_dijet_forward"),
+        #                                       subplot_type="ratio",
+        #                                       subplot_title="Variation / nominal",
+        #                                       subplot_limits=subplot_lim,
+        #                                       do_flav_tagged=False,
+        #                                       has_data=False)
 
 
+        qgp.do_all_exclusive_plots_comparison(sources,
+                                              var_list=qgc.COMMON_VARS,
+                                              pt_bins=qgc.PT_BINS,
+                                              qcd_filename=qgc.QCD_FILENAME,
+                                              dj_cen_dirname=None,
+                                              dj_fwd_dirname="Dijet_QG_tighter",
+                                              zpj_dirname=None,
+                                              plot_dir=os.path.join(output_dir, "plots_qcd_compare_dijet"),
+                                              subplot_type="ratio", # will use the 1st entry by default
+                                              subplot_title="Variation / nominal",
+                                              subplot_limits=subplot_lim,
+                                              do_flav_tagged=False,
+                                              has_data=False,
+                                              title=qgc.Dijet_LABEL,
+                                              show_region_labels=False)
+
+
+    # COMPARE DY
+    if os.path.isfile(os.path.join(nominal_dir, qgc.DY_FILENAME)):
+        sources = [
+           {
+               "root_dir": nominal_dir,
+               'label': "Nominal MG5+Pythia8",
+               "style": {
+                   'line_style': 1,
+                   'line_width': lw,
+                   'line_color': qgc.DY_COLOUR,
+                   'fill_color': qgc.DY_COLOUR,
+                   'marker_color': qgc.DY_COLOUR,
+                   'marker_style': cu.Marker.get(qgc.DY_MARKER),
+                   'marker_size': 0,
+               }
+
+           }
+        ]
+
+        # Add systematics
+        # SHIFT UP
+        if syst_ups:
+            for ind, (syst_up_dir, syst_up_label, syst_up_col) in enumerate(syst_ups):
+                this_source = deepcopy(sources[0])
+                this_source['root_dir'] = syst_up_dir
+                this_source.update(
+                    dict(style=dict(line_color=syst_up_col, line_width=lw, fill_color=syst_up_col, line_style=2,
+                                     marker_color=syst_up_col, marker_style=cu.Marker.get('triangleUp'), marker_size=0),
+                         label=syst_up_label,
+                         subplot=0)
+                )
+                sources.append(this_source)
+
+        # SHIFT DOWN
+        if syst_downs:
+            for ind, (syst_down_dir, syst_down_label, syst_down_col) in enumerate(syst_downs):
+                this_source = deepcopy(sources[0])
+                this_source['root_dir'] = syst_down_dir
+                this_source.update(
+                    dict(style=dict(line_color=syst_down_col, line_width=lw, fill_color=syst_down_col, line_style=3,
+                                    marker_color=syst_down_col, marker_style=cu.Marker.get('triangleDown'), marker_size=0),
+                         label=syst_down_label, subplot=0)
+                )
+                sources.append(this_source)
+
+        subplot_lim = (0.75, 1.25)
+
+        qgp.do_all_exclusive_plots_comparison(sources,
+                                              var_list=qgc.COMMON_VARS,
+                                              pt_bins=qgc.PT_BINS,
+                                              qcd_filename=qgc.DY_FILENAME,
+                                              dj_cen_dirname=None,
+                                              dj_fwd_dirname=None,
+                                              zpj_dirname="ZPlusJets_QG",
+                                              plot_dir=os.path.join(output_dir, "plots_dy_compare"),
+                                              subplot_type="ratio",
+                                              subplot_title="Variation / nominal",
+                                              subplot_limits=subplot_lim,
+                                              do_flav_tagged=False,
+                                              has_data=False,
+                                              title=qgc.ZpJ_LABEL,
+                                              show_region_labels=False)
 
 
 if __name__ == "__main__":
