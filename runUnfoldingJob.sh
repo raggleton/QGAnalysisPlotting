d37b1ff8f8c0cfe6896cde9e47b3ae33d7abdadd
Robin Aggleton <robin.aggleton@cern.ch>
2021-01-21 18:40:54 +0000

    Update unfolding job script

--- runUnfoldingJob.sh
+++ runUnfoldingJob.sh
@@ -9,14 +9,7 @@ export LD_LIBRARY_PATH=""
 
 # Setup CMSSW etc
 CMSSW_AREA=/nfs/dust/cms/user/aggleton/QG/102X/CMSSW_10_2_10/src
-# cmssw setup triggers this err so turn it off for now
-#set +u
-#source /cvmfs/cms.cern.ch/cmsset_default.sh
-#cd ${CMSSW_AREA}
-#eval `scramv1 runtime -sh`
-#cd ${CMSSW_AREA}/UHH2/QGAnalysisPlotting/
-#printenv | sort
-#set -u
+
 # To get conda
 # Can't use $HOME because grid-control manges my env vars
 source /afs/desy.de/user/a/aggleton/.bashrc
@@ -27,25 +20,58 @@ set -x
 # INPUTDIR and OUTPUTDIR come from the grid-control config
 ODIR="${INPUTDIR}/${OUTPUTDIR}"
 
+COMMON_ARGS="$INPUTDIR --outputDir=$ODIR --do$CHANNEL --angles $ANGLE --noBinnedPlots"
+
+# MC input, all MC
+# ------------------------------------------------------------------------------
+#python unfolding.py ${COMMON_ARGS} --MCinput=True --MCsplit=False
+
 # MC input, split (closure test), and alt response
-python unfolding.py $INPUTDIR --outputDir=$ODIR --do$CHANNEL --regularize=None --MCinput=True --MCsplit=True --useAltResponse=True --angles $ANGLE --noBinnedPlots
+# ------------------------------------------------------------------------------
+#python unfolding.py ${COMMON_ARGS} --MCinput=True --MCsplit=True --useAltResponse=True
 
 # MC input, not split - use to derive model, exp, pdf systs for data
-ARGS="$INPUTDIR --outputDir=$ODIR --do$CHANNEL --regularize=None --MCinput=True --MCsplit=False --useAltResponse=False --doExperimentalSysts=True --doScaleSysts=True --doPDFSysts=True --angles $ANGLE --noBinnedPlots"
+# ------------------------------------------------------------------------------
+# Do some per-variable/channel settings
+EXP_SYST_AS_ALT_RSP=0
+MERGE_LAST_PT_BIN=0
+ZERO_BAD_RSP_BIN=0
+if [[ "$CHANNEL" == *"Dijet"* ]]; then
+    EXP_SYST_AS_ALT_RSP=1
+else
+    if [[ "ANGLE" == *"Multiplicity"* ]]; then
+        MERGE_LAST_PT_BIN=1
+        ZERO_BAD_RSP_BIN=1
+    fi
+fi
+
+ARGS="${COMMON_ARGS} --MCinput=True "\
+"--doExperimentalSysts=True --doExperimentalSystsAsAltResponse ${EXP_SYST_AS_ALT_RSP} "\
+"--doScaleSysts=True --doPDFSysts=True "\
+"--mergeLastPtBin ${MERGE_LAST_PT_BIN} --zeroBadResponseBins ${ZERO_BAD_RSP_BIN}"
+
 # stash location of output file for use later with data
 REFFILE=$(python unfolding_logistics.py out $ARGS)
 python unfolding.py $ARGS
 
-# Data input, use previous MC file to get systs
-python unfolding.py $INPUTDIR --outputDir=$ODIR --do$CHANNEL --regularize=None --MCinput=False --MCsplit=False --useAltResponse=False --doExperimentalSystsFromFile=$REFFILE --doScaleSystsFromFile=$REFFILE --doPDFSystsFromFile=$REFFILE --angles $ANGLE --noBinnedPlots
+# Data input, use previous MC file to get systs if necessary
+# ------------------------------------------------------------------------------
+EXP_SYSTS_ARGS="--doExperimentalSystsFromFile=$REFFILE"
+if [[ "$CHANNEL" == *"Dijet"* ]]; then
+    EXP_SYSTS_ARGS="--doExperimentalSysts=True --doExperimentalSystsAsAltResponse=${EXP_SYST_AS_ALT_RSP}"
+fi
+
+ARGS="${COMMON_ARGS} "\
+"${EXP_SYSTS_ARGS} "\
+"--doScaleSystsFromFile=$REFFILE "\
+"--doPDFSystsFromFile=$REFFILE "\
+"--mergeLastPtBin ${MERGE_LAST_PT_BIN} --zeroBadResponseBins ${ZERO_BAD_RSP_BIN}"
+
+python unfolding.py $ARGS
 
 # Data input, use alt response matrix as cross-check
-python unfolding.py $INPUTDIR --outputDir=$ODIR --do$CHANNEL --regularize=None --MCinput=False --MCsplit=False --useAltResponse=True --angles $ANGLE --noBinnedPlots
+# ------------------------------------------------------------------------------
+#python unfolding.py ${COMMON_ARGS} --useAltResponse=True
 
-# Zip up output files
-# Not needed?
-#cd $ODIR && tar cvzf unfolding_${NICKNAME}.tar.gz ${OUTPUTDIR1#$ODIR/} ${OUTPUTDIR2#$ODIR/} ${OUTPUTDIR3#$ODIR/} ${OUTPUTDIR4#$ODIR/} && sleep 10 && cd ..
-#cd $ODIR && tar cvzf unfolding_${CHANNEL}_${MCINPUT}_${MCSPLIT}_${ANGLE}.tar.gz $ODIR/* && sleep 10 && cd ..
 set +x
 set +u
-
